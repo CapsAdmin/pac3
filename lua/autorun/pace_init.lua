572d0db00a06325451afbab60eb9669fa75a69c7
pbninja101@gmail.com <pbninja101@gmail.com@047d434e-d786-fb00-e516-99c5e643cd71>
2012-05-30 18:02:47 +0000

    Metastruct fix. Won't affect other servers.
    git-svn-id: http://gmodcapsadmin.googlecode.com/svn/trunk/addons/pac3@21 047d434e-d786-fb00-e516-99c5e643cd71

--- lua/autorun/pace_init.lua
+++ lua/autorun/pace_init.lua
@@ -5,12 +5,11 @@ if SERVER then
 		local files
 		
 		if net then
-			local _files, _folders = file.Find(dir .. "*", LUA_PATH)
+			local _files, _folders = file.FindNewBeta and file.FindNewBeta(dir .. "*", LUA_PATH) or file.Find(dir .. "*", LUA_PATH)
 			files = {}
 			for k,v in pairs(_files) do 
 				table.insert(files, v)
 			end
-			
 			for k,v in pairs(_folders) do 
 				table.insert(files, v)
 			end
