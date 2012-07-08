4048f59e9d364d89ee54500ab32980d5011a1bb6
eliashogstvedt@gmail.com <eliashogstvedt@gmail.com@047d434e-d786-fb00-e516-99c5e643cd71>
2012-07-08 10:46:20 +0000

    git-svn-id: http://gmodcapsadmin.googlecode.com/svn/trunk/addons/pac3@93 047d434e-d786-fb00-e516-99c5e643cd71

--- lua/autorun/pace_init.lua
+++ lua/autorun/pace_init.lua
@@ -1,7 +1,5 @@
 if SERVER then
-	AddCSLuaFile("autorun/pace_init.lua")
-		
-	local function add_files(dir)
+	local function getfiles(dir)
 		local files
 		
 		if net then
@@ -10,7 +8,13 @@ if SERVER then
 			files = file.FindInLua(dir .. "*")
 		end
 		
-		for _, name in pairs(files) do
+		return files
+	end
+	
+	AddCSLuaFile("autorun/pace_init.lua")
+		
+	local function add_files(dir)		
+		for _, name in pairs(getfiles(dir)) do
 			if name:sub(-4) == ".lua" then
 				AddCSLuaFile(dir .. name)
 			elseif not name:find(".", nil, true) then
@@ -18,8 +22,12 @@ if SERVER then
 			end
 		end
 	end
-
+	
 	add_files("pac3/pace/")
+
+	for _, name in pairs(getfiles("pac3/pace/translations/")) do
+		resource.AddSingleFile("pac3/pace/translations/" .. name)
+	end
 end
 
 if CLIENT then
