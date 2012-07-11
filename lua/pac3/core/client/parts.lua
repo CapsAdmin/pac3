ebedb2668ced36c50b3bf457f0a4a5957f653bca
CapsAdmin <eliashogstvedt@gmail.com>
2012-07-11 17:08:49 +0000

    git-svn-id: http://gmodcapsadmin.googlecode.com/svn/trunk/addons/pac3@110 047d434e-d786-fb00-e516-99c5e643cd71

--- lua/pac3/core/client/parts.lua
+++ lua/pac3/core/client/parts.lua
@@ -562,7 +562,7 @@ do -- meta
 
 			for _, key in pairs(self:GetStorableVars()) do
 				tbl.self[key] = COPY(self["Get"..key] and self["Get"..key](self) or self[key])
-				if make_copy_name and key:find("Name", nil, true) then
+				if make_copy_name and key == "Name" or is_child and key == "ParentName" then
 					tbl.self[key] = tbl.self[key] .. " copy"
 				end
 			end
