ec3e4969599f68eb79520652393a4dc2a67cca69
CapsAdmin <eliashogstvedt@gmail.com>
2012-08-03 22:21:20 +0000

    git-svn-id: http://gmodcapsadmin.googlecode.com/svn/trunk/addons/pac3@182 047d434e-d786-fb00-e516-99c5e643cd71

--- lua/pac3/core/client/wear.lua
+++ lua/pac3/core/client/wear.lua
@@ -13,13 +13,13 @@ do -- to server
 		end
 	end
 
-	function pac.RemovePartOnServer(name)
+	function pac.RemovePartOnServer(name, server_only)
 		if VERSION >= 150 then
 			net.Start("pac_submit")
-				net.WriteTable({part = name})
+				net.WriteTable({part = name, server_only = server_only})
 			net.SendToServer()
 		else
-			datastream.StreamToServer("pac_submit", {part = name})
+			datastream.StreamToServer("pac_submit", {part = name, server_only = server_only})
 		end
 	end
 end
