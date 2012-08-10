833c8e81abbaaae68da4d89cf16f5f7cb243d4f8
eliashogstvedt@gmail.com <eliashogstvedt@gmail.com@047d434e-d786-fb00-e516-99c5e643cd71>
2012-08-10 13:37:26 +0000

    git-svn-id: http://gmodcapsadmin.googlecode.com/svn/trunk/addons/pac3@211 047d434e-d786-fb00-e516-99c5e643cd71

--- lua/pac3/core/client/libraries/urlmat.lua
+++ lua/pac3/core/client/libraries/urlmat.lua
@@ -9,6 +9,7 @@ if urlmat.ActivePanel:IsValid() then
 end
 
 function urlmat.GetMaterialFromURL(url, callback)	
+	PrintTable(urlmat.Queue)
 	if urlmat.Queue[url] then
 		local old = urlmat.Queue[url].callback
 		urlmat.Queue[url].callback = function(...)	
@@ -28,6 +29,9 @@ function urlmat.Think()
 				urlmat.StartDownload(url, data)
 			end
 		end
+		urlmat.Busy = true
+	else
+		urlmat.Busy = false
 	end
 end
 
@@ -43,93 +47,99 @@ function urlmat.StartDownload(url, data)
 	
 	local pnl = vgui.Create("HTML")
 	pnl:SetVisible(true)
-	pnl:KillFocus()
-	pnl:SetPos(ScrW()-1, ScrH()-1)
+--	pnl:SetPos(ScrW()-1, ScrH()-1)
 	pnl:SetSize(urlmat.TextureSize, urlmat.TextureSize)
-	pnl:SetHTML(
-		[[
-			<style type="text/css">
-				html 
-				{			
-					overflow:hidden;
-				}
-			</style>
-			
-			<body>
-				<img src="]] .. url .. [[" alt="" width="]] .. urlmat.TextureSize..[[" height="]] .. urlmat.TextureSize .. [[" />
-			</body>
-		]]
-	)
-		
-	local function start()
-		local go = false
-		local time = 0
-
-		-- restart the timeout
-		timer.Stop(id)
-		timer.Start(id)
 	
-		hook.Add("Think", id, function()
+	function pnl:Paint() -- hacky delayed loading..
+		if self.__loaded then return end
+		self.__loaded = true
+		self.Paint=nil
 		
-			-- panel is no longer valid
-			if not pnl:IsValid() then
-				hook.Remove("Think", id)
-				-- let the timeout handle it
-				return
-			end
-			
-			local html_mat = pnl:GetHTMLMaterial()
-					
-			-- give it some time.. IsLoading is sometimes lying
-			if not go and html_mat and not pnl:IsLoading() then
-				time = RealTime() + 0.15
-				go = true
-			end
-			
-			if go and time < RealTime() then
-				local vertex_mat = CreateMaterial(url, "VertexLitGeneric")
-				local tex = html_mat:GetMaterialTexture("$basetexture")
-				vertex_mat:SetMaterialTexture("$basetexture", tex)
-
-				hook.Remove("Think", id)
-				timer.Remove(id)
-				urlmat.Queue[url] = nil
-				pnl:Remove()
+		pnl:SetHTML(
+			[[
+				<style type="text/css">
+					html 
+					{			
+						overflow:hidden;
+					}
+				</style>
 				
-				if data.callback then
-					data.callback(vertex_mat, tex)
-				end
-			end
+				<body>
+					<img src="]] .. url .. [[" alt="" width="]] .. urlmat.TextureSize..[[" height="]] .. urlmat.TextureSize .. [[" />
+				</body>
+			]]
+		)
 			
+		local function start()
+			local go = false
+			local time = 0
+
+			-- restart the timeout
+			timer.Stop(id)
+			timer.Start(id)
+		
+			hook.Add("Think", id, function()
+			
+				-- panel is no longer valid
+				if not pnl:IsValid() then
+					hook.Remove("Think", id)
+					-- let the timeout handle it
+					return
+				end
+				
+				local html_mat = pnl:GetHTMLMaterial()
+						
+				-- give it some time.. IsLoading is sometimes lying
+				if not go and html_mat and not pnl:IsLoading() then
+					time = RealTime() + 0.25
+					go = true
+				end
+				
+				if go and time < RealTime() then
+					local vertex_mat = CreateMaterial(url, "VertexLitGeneric")
+					local tex = html_mat:GetMaterialTexture("$basetexture")
+					vertex_mat:SetMaterialTexture("$basetexture", tex)
+
+					hook.Remove("Think", id)
+					timer.Remove(id)
+					urlmat.Queue[url] = nil
+					timer.Simple(0.25, function() pnl:Remove() end)
+					
+					if data.callback then
+						data.callback(vertex_mat, tex)
+					end
+				end
+				
+			end)
+		end
+		
+		if VERSION >= 150 then
+			start()
+		else
+			pnl.FinishedURL = start
+		end
+		
+		-- 5 sec max timeout
+		timer.Create(id, 5, 1, function()
+			timer.Remove(id)
+			urlmat.Queue[url] = nil
+			pnl:Remove()
+			
+			if hook.GetTable().Think[id] then
+				hook.Remove("Think", id)
+			end
+
+			if data.tries < 3 then
+				pac.dprint("material download %q timed out.. trying again for the %ith time", url, data.tries)
+				-- try again
+				data.tries = data.tries + 1
+				urlmat.GetMaterialFromURL(url, data)
+				urlmat.Queue[url] = data
+			else
+				pac.dprint("material download %q timed out for good", url, data.tries)
+			end
 		end)
 	end
-	
-	if VERSION >= 150 then
-		start()
-	else
-		pnl.FinishedURL = start
-	end
-	
-	-- 5 sec max timeout
-	timer.Create(id, 5, 1, function()
-		timer.Remove(id)
-		urlmat.Queue[url] = nil
-		pnl:Remove()
-		
-		if hook.GetTable().Think[id] then
-			hook.Remove("Think", id)
-		end
-
-		if data.tries < 3 then
-			pac.dprint("material download %q timed out.. trying again for the %ith time", url, data.tries)
-			-- try again
-			data.tries = data.tries + 1
-			urlmat.GetMaterialFromURL(url, data)
-			urlmat.Queue[url] = data
-		else
-			pac.dprint("material download %q timed out for good", url, data.tries)
-		end
-	end)
 	
 	urlmat.ActivePanel = pnl
 end
