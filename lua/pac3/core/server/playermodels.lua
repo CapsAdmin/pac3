db33d7b8b3e8f5f450c84bb09c6542fd157553a5
eliashogstvedt@gmail.com <eliashogstvedt@gmail.com@047d434e-d786-fb00-e516-99c5e643cd71>
2012-08-11 22:52:04 +0000

    git-svn-id: http://gmodcapsadmin.googlecode.com/svn/trunk/addons/pac3@217 047d434e-d786-fb00-e516-99c5e643cd71

--- lua/pac3/core/server/playermodels.lua
+++ lua/pac3/core/server/playermodels.lua
@@ -2,9 +2,9 @@ timer.Create("pac_playermodels", 0.25, 0, function()
 	for key, ply in pairs(player.GetAll()) do
 		local mdl = ply:GetInfo("cl_playermodel")
 		if ply.pac_last_model ~= mdl then
-			mdl = player_manager.TranslatePlayerModel(mdl)
-			if mdl then
-				ply:SetModel(mdl)
+			local realmdl = player_manager.TranslatePlayerModel(mdl)
+			if realmdl then
+				ply:SetModel(realmdl)
 			end
 			ply.pac_last_model = mdl
 		end
