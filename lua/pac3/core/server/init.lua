166551e0ec8a6f67e8d3a7886421dcfa33c8a139
CapsAdmin <eliashogstvedt@gmail.com>
2012-11-22 23:29:39 +0000

    git-svn-id: http://gmodcapsadmin.googlecode.com/svn/trunk/addons/pac3@374 047d434e-d786-fb00-e516-99c5e643cd71

--- lua/pac3/core/server/init.lua
+++ lua/pac3/core/server/init.lua
@@ -21,12 +21,4 @@ concommand.Add("pac_in_editor", function(ply, _, args)
 	ply:SetNWBool("in pac3 editor", tonumber(args[1]) == 1)
 end)
 
-local tags = GetConVarString("sv_tags")
-
-if tags == "" then
-	RunConsoleCommand("sv_tags", "PAC3")
-elseif not tags:find("PAC3") then
-	RunConsoleCommand("sv_tags", "PAC3," .. tags)
-end
-
 CreateConVar("has_pac3", "1", {FCVAR_NOTIFY})
