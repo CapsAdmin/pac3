217607475d194ca837b9dab16e9f539f036a5b2e
eliashogstvedt@gmail.com <eliashogstvedt@gmail.com@047d434e-d786-fb00-e516-99c5e643cd71>
2012-12-04 18:19:44 +0000

    git-svn-id: http://gmodcapsadmin.googlecode.com/svn/trunk/addons/pac3@408 047d434e-d786-fb00-e516-99c5e643cd71

--- lua/pac3/core/client/base_part.lua
+++ lua/pac3/core/client/base_part.lua
@@ -157,7 +157,7 @@ do -- owner
 		end
 		
 		-- this line keeps lying telling me the player is valid, but when trying to use it, gmod throws an error
-		if self.Owner:IsPlayer() and not self.Owner:Alive() then
+		if self.Owner:IsPlayer() and self.Owner:IsValid() and not self.Owner:Alive() then
 			local rag = self.Owner:GetRagdollEntity()
 			if rag and rag:IsValid() then
 				return rag
