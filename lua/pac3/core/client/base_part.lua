c8fa0f6e0ef0fbae9a5697e30082e508aa41b0e7
CapsAdmin <eliashogstvedt@gmail.com>
2013-02-17 23:08:52 +0000

    git-svn-id: http://gmodcapsadmin.googlecode.com/svn/trunk/addons/pac3@544 047d434e-d786-fb00-e516-99c5e643cd71

--- lua/pac3/core/client/base_part.lua
+++ lua/pac3/core/client/base_part.lua
@@ -697,6 +697,13 @@ function PART:CalcAngles(owner, ang)
 	
 	if pac.StringFind(self.AimPartName, "LOCALEYES", true, true) then
 		return self.Angles + (pac.EyePos - self.cached_pos):Angle()
+	elseif pac.StringFind(self.AimPartName, "playereyes", true, true) then
+		local ent = me.pac_traceres.Entity
+		if ent:IsValid() then
+			return self.Angles + (ent:EyePos() - self.cached_pos):Angle()
+		else
+			return self.Angles + (pac.EyePos - self.cached_pos):Angle()
+		end
 	elseif self.AimPart:IsValid() then	
 		return self.Angles + (self.AimPart.cached_pos - self.cached_pos):Angle()
 	elseif self.EyeAngles then
