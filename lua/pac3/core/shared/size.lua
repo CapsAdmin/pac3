8b52cdaf99c85314a77e8a73a7f813a35d5d6f72
eliashogstvedt@gmail.com <eliashogstvedt@gmail.com@047d434e-d786-fb00-e516-99c5e643cd71>
2013-02-17 12:25:09 +0000

    git-svn-id: http://gmodcapsadmin.googlecode.com/svn/trunk/addons/pac3@541 047d434e-d786-fb00-e516-99c5e643cd71

--- lua/pac3/core/shared/size.lua
+++ lua/pac3/core/shared/size.lua
@@ -56,9 +56,7 @@ function pac.SetPlayerSize(ply, f)
 		hook.Add("Think", "pac_check_scale", function()
 			for key, ply in pairs(player.GetAll()) do 
 				local siz = ply.pac_player_size or 1
-				
-				print(ply, ply:GetModelScale(), ply:GetViewOffset(), def.view * siz, siz)
-				
+								
 				if siz ~= 1 and (ply:GetModelScale() ~= siz or ply:GetViewOffset() ~= def.view * siz) then
 					pac.SetPlayerSize(ply, siz)
 				end
