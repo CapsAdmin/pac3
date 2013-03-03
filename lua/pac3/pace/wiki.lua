4e8add085eb22f327f88a58760ae0e2d2ca049cd
eliashogstvedt@gmail.com <eliashogstvedt@gmail.com@047d434e-d786-fb00-e516-99c5e643cd71>
2013-03-03 13:10:17 +0000

    git-svn-id: http://gmodcapsadmin.googlecode.com/svn/trunk/addons/pac3@580 047d434e-d786-fb00-e516-99c5e643cd71

--- lua/pac3/pace/wiki.lua
+++ lua/pac3/pace/wiki.lua
@@ -1,3 +1,4 @@
+pace.WikiURL = "http://pac.metastruct.org"
 pace.wiki_cache = {}
 
 function pace.GetPropertyDescription(part, field, callback)
@@ -8,7 +9,7 @@ function pace.GetPropertyDescription(part, field, callback)
 	if pace.wiki_cache[part] then
 		 go(pace.wiki_cache[part])
 	end
-	http.Fetch("http://pac.educatewiki.com/wiki/Part_"..part, function(s)	
+	http.Fetch(pace.WikiURL .. "/index.php/Part_"..part, function(s)	
 		pace.wiki_cache[part] = pace.wiki_cache[part] or s
 		go(s)
 	end)
@@ -17,6 +18,6 @@ end
 
 function pace.ShowHelp(part)
 	if part then
-		pace.ShowWiki("http://pac.educatewiki.com/w/index.php?title=Part_" .. part .. "&action=view")
+		pace.ShowWiki(pace.WikiURL .. "/index.php?title=Part_" .. part .. "&action=view")
 	end
 end
