f1babf5a1c91a894119454054dbdca0a89dd86bc
CapsAdmin <eliashogstvedt@gmail.com>
2013-03-26 18:53:25 +0000

    git-svn-id: http://gmodcapsadmin.googlecode.com/svn/trunk/addons/pac3@662 047d434e-d786-fb00-e516-99c5e643cd71

--- lua/pac3/core/shared/init.lua
+++ lua/pac3/core/shared/init.lua
@@ -6,3 +6,9 @@ include("pac_weapon.lua")
 include("modifiers.lua")
 include("footsteps_fix.lua")
 include("projectiles.lua")
+
+local cvar = CreateConVar("pac_restrictions", "0", FCVAR_REPLICATED)
+
+function pac.GetRestrictionLevel()
+	return cvar:GetInt()
+end
