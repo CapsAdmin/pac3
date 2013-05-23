cb59b0a4f98c0f954d7041d6edc00e84d91aeaa1
CapsAdmin <eliashogstvedt@gmail.com>
2013-05-23 17:25:03 +0200

    fixed spawn as contraption made it so the weapon class tool doesn't set the name of the part (since it does this nicely internally)

--- lua/pac3/editor/client/contraption.lua
+++ lua/pac3/editor/client/contraption.lua
@@ -1,24 +1,25 @@
-function pac.PartToContraptionData(part, tbl)	
+function pace.PartToContraptionData(part, tbl)	
 	tbl = tbl or {}
 		
 	if part.ClassName == "model" then
 		local data = {}
 		
 		local c = part:GetColor()
-		local a = part:GetAlpha() * 255
-		data.clr = {c.r, c.g, c.b, a}
+
+		data.clr = {c.r, c.g, c.b, part:GetAlpha() * 255}
 		data.ang = part.Entity:GetAngles()
 		data.pos = part.Entity:GetPos()
 		data.mat = part:GetMaterial()
 		data.mdl = part:GetModel()
 		data.skn = part:GetSkin()
+		data.id = part.UniqueID
 			
 		table.insert(tbl, data)
 	end
 	
 	for key, part in pairs(part:GetChildren()) do
 		if part.ClassName == "model" then
-			pac.PartToContraptionData(part, tbl)
+			pace.PartToContraptionData(part, tbl)
 		end
 	end
 	
--- lua/pac3/editor/server/init.lua
+++ lua/pac3/editor/server/init.lua
@@ -8,5 +8,6 @@ include("bans.lua")
 include("event.lua")
 include("effects.lua")
 include("wear.lua")
+include("contraption.lua")
 
 CreateConVar("has_pac3_editor", "1", {FCVAR_NOTIFY})
