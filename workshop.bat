75323dd8a3585f11310799529269bd2c78f4b2ae
CapsAdmin <eliashogstvedt@gmail.com>
2013-08-17 13:43:05 +0200

    added workshop.bat to easily update the workshop version

---
+++ workshop.bat
@@ -0,0 +1,9 @@
+set WORKSHOP_ID=104691717
+
+cd /d "%~dp0"
+cd ../../../bin/
+
+gmad create -folder "%~dp0\" -out "%~dp0\..\__TEMP.gma"
+gmpublish update -addon "%~dp0\..\__TEMP.gma" -id "%WORKSHOP_ID%"
+
+pause
