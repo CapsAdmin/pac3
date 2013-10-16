14f431343a94981290af6edbda59f5d2b7a4d702
CapsAdmin <eliashogstvedt@gmail.com>
2013-10-16 17:11:17 +0200

    experimental new obj parser normals are now generated from pac itself (seems to fix black triangles!!!!!!!!!!!!!!) obj generation is now spread out in frames to reduce the amount of lagspikes

--- lua/pac3/core/client/libraries/urlobj.lua
+++ lua/pac3/core/client/libraries/urlobj.lua
@@ -13,117 +13,169 @@ end)
 local table_insert = table.insert
 local tonumber = tonumber
 
-function urlobj.ParseObj(data, merge_models, hack)
-	debug.sethook()
 
-	local positions = {}
-	local texcoords = {}
-	local normals = {}
-	local outputs = {}
-	
-	if pac.debug then
-		debug.Trace()
-	end
-	pac.dprint("parsing model")
-	
-	local lines = {}
-	
-	for i in data:gmatch("(.-)\n") do
-		local parts = i:gsub("%s+", " "):Trim():Split(" ")
+jit.on(parse)
+jit.flush(parse)
 
-		table.insert(lines, parts)
-	end
+local i = 0 
+
+function urlobj.ParseObj(data, generate_normals, callback, dbgprint)
+	local co = coroutine.create(function()
+		debug.sethook()
+
+		local positions = {}
+		local texcoords = {}
+		local normals = {}
 		
-	for _, parts in pairs(lines) do		
-		if parts[1] == "v" and #parts >= 4 then
-			table_insert(positions, Vector(parts[2], parts[3], parts[4]))
-		elseif parts[1] == "vt" and #parts >= 3 then
-			table_insert(texcoords, tonumber(parts[2]))
-			table_insert(texcoords, tonumber(1 - parts[3]))
-		elseif parts[1] == "vn" and #parts >= 4 then
-			table_insert(normals, Vector(parts[2], parts[3], parts[4]))
+		local output = {}
+		
+		local lines = {}
+		
+		local i = 1
+		for line in data:gmatch("(.-)\n") do
+			local parts = line:gsub("%s+", " "):Trim():Split(" ")
+
+			table_insert(lines, parts)
+			coroutine.yield(false, "inserting lines", i)
+			i = i + 1
 		end
-	end
-		
-	local submodel
-		
-	for _, parts in pairs(lines) do
-		if parts[1] == "g" and not merge_models then
-			submodel = {}
-			outputs[parts[2]] = submodel
-		elseif parts[1] == "f" and #parts > 3 then
-			local first, previous
-
-			for i = 2, #parts do
-				local current = parts[i]:Split("/")
-
-				if i == 2 then
-					first = current
-				end
+	
+		local vert_count = #lines
+	
+		for i, parts in pairs(lines) do		
+			if parts[1] == "v" and #parts >= 4 then
+				table_insert(positions, Vector(tonumber(parts[2]), tonumber(parts[3]), tonumber(parts[4])))
+			elseif parts[1] == "vt" and #parts >= 3 then
+				table_insert(texcoords, {u = tonumber(parts[2])%1, v = tonumber(1-parts[3])%1})
+			elseif not generate_normals and parts[1] == "vn" and #parts >= 4 then
+				table_insert(normals, Vector(tonumber(parts[2]), tonumber(parts[3]), tonumber(parts[4])):GetNormalized())
+			end
+			
+			coroutine.yield(false, "parsing lines", (i/vert_count))
+		end
 				
-				if i >= 4 then
-					local v1, v2, v3 = {}, {}, {}
+		for i, parts in pairs(lines) do
+			if parts[1] == "f" and #parts > 3 then
+				local first, previous
 
-					v1.pos = positions[tonumber(first[1])]
-					v2.pos = positions[tonumber(current[1])]
-					v3.pos = positions[tonumber(previous[1])]
+				for i = 2, #parts do
+					local current = parts[i]:Split("/")
 
-					if #normals > 0 then
-						v1.normal = normals[tonumber(first[3])]
-						v2.normal = normals[tonumber(current[3])]
-						v3.normal = normals[tonumber(previous[3])]
+					if i == 2 then
+						first = current
 					end
 					
-					if #texcoords > 0 then
-						v1.u = texcoords[1 + (tonumber(first[2]) - 1) * 2 + 0]%1
-						v1.v = texcoords[1 + (tonumber(first[2]) - 1) * 2 + 1]%1
+					if i >= 4 then
+						local v1, v2, v3 = {}, {}, {}
+
+						v1.pos_index = tonumber(first[1])
+						v2.pos_index = tonumber(current[1])
+						v3.pos_index = tonumber(previous[1])
 						
-						v2.u = texcoords[1 + (tonumber(current[2]) - 1) * 2 + 0]%1
-						v2.v = texcoords[1 + (tonumber(current[2]) - 1) * 2 + 1]%1
+						v1.pos = positions[tonumber(first[1])]
+						v2.pos = positions[tonumber(current[1])]
+						v3.pos = positions[tonumber(previous[1])]
 						
-						v3.u = texcoords[1 + (tonumber(previous[2]) - 1) * 2 + 0]%1
-						v3.v = texcoords[1 + (tonumber(previous[2]) - 1) * 2 + 1]%1
-					end
-					
-					if not submodel then
-						submodel = {}
-						outputs["none"] = submodel
+						if #texcoords > 0 then
+							v1.u = texcoords[tonumber(first[2])].u
+							v1.v = texcoords[tonumber(first[2])].v
+							
+							v2.u = texcoords[tonumber(current[2])].u
+							v2.v = texcoords[tonumber(current[2])].v
+							
+							v3.u = texcoords[tonumber(previous[2])].u
+							v3.v = texcoords[tonumber(previous[2])].v
+						end
+						
+						if #normals > 0 then
+							v1.normal = normals[tonumber(first[3])]
+							v2.normal = normals[tonumber(current[3])]
+							v3.normal = normals[tonumber(previous[3])]
+						end				
+						
+						table_insert(output, v1)
+						table_insert(output, v2)
+						table_insert(output, v3)
 					end
 
-					table_insert(submodel, v1)
-					table_insert(submodel, v2)
-					table_insert(submodel, v3)
-					
-					if hack then
-						table_insert(submodel, v1)
-						table_insert(submodel, v2)
-						table_insert(submodel, v3)
-					end
+					previous = current
 				end
+			end
+			
+			coroutine.yield(false, "solving indices", i/vert_count)
+		end
+		
+		if generate_normals then
+			local vertex_normals = {}
+			local count = #output/3
+			for i = 1, count do
+				local a, b, c = output[1+(i-1)*3+0], output[1+(i-1)*3+1], output[1+(i-1)*3+2] 
+				local normal = (c.pos - a.pos):Cross(b.pos - a.pos):GetNormalized()
 
-				previous = current
+				vertex_normals[a.pos_index] = vertex_normals[a.pos_index] or Vector()
+				vertex_normals[a.pos_index] = (vertex_normals[a.pos_index] + normal)
+
+				vertex_normals[b.pos_index] = vertex_normals[b.pos_index] or Vector()
+				vertex_normals[b.pos_index] = (vertex_normals[b.pos_index] + normal)
+
+				vertex_normals[c.pos_index] = vertex_normals[c.pos_index] or Vector()
+				vertex_normals[c.pos_index] = (vertex_normals[c.pos_index] + normal)
+				coroutine.yield(false, "generating normals", i/count)
+			end
+			
+			local default_normal = Vector(0, 0, -1)
+
+			local count = #output
+			for i = 1, count do
+				local n = vertex_normals[output[i].pos_index] or default_normal
+				n:Normalize()
+				normals[i] = n
+				output[i].normal = n
+				coroutine.yield(false, "smoothing normals", i/count)
 			end
 		end
-	end
+		
+		callback(output)
+		coroutine.yield(true)
+	end)
 
-	return outputs
+	local last_why
+	local id = "pac_parse_obj_" .. i
+	
+	hook.Add("Think", id, function()
+		for i = 1, 512 do
+			local dead, done, why, msg = coroutine.resume(co)
+			if done then
+				if dead == false and done then
+					if dbgprint then dbgprint(done) end
+					hook.Remove("Think", id)
+				end
+				return true
+			else
+				if dbgprint and last_why ~= why and msg then
+					if why == "inserting lines" then
+						dbgprint(why .. " " .. msg, 2)
+					else
+						dbgprint(why .. " " .. math.round(msg*100) .. " %", 2)
+					end
+					
+					last_why = why
+				end
+			end
+		end
+	end)
+	
+	i = i + 1
 end
 
 function urlobj.CreateObj(obj_str, merge_models, hack)	
-	local ok, res = pcall(urlobj.ParseObj, obj_str, merge_models, hack)
+	local mesh = Mesh()
 	
-	if not ok then
-		MsgN("pac3 obj parse error %q ", res)
-		return
-	end
-
-	for name, data in pairs(res) do
-		local mesh = Mesh()
+	urlobj.ParseObj(obj_str, true, function(data)
 		mesh:BuildFromTriangles(data)
-		res[name] = mesh
-	end
+	end)
 	
-	return res
+	return {mesh}
 end
 
 local enable = CreateClientConVar("pac_enable_urlobj", "1", true)
