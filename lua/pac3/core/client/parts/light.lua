208039b24b2f83a6a74e6b446f387a3ac36c1162
CapsAdmin <eliashogstvedt@gmail.com>
2013-11-14 09:57:33 +0100

    light fix attempt

--- lua/pac3/core/client/parts/light.lua
+++ lua/pac3/core/client/parts/light.lua
@@ -16,38 +16,32 @@ end
 
 local DynamicLight = DynamicLight
 
-pac.dynamic_lights = pac.dynamic_lights or {}
-
 function PART:OnDraw(owner, pos, ang)
-	local id = tonumber(self.UniqueID)
-	self.Params = self.Params or pac.dynamic_lights[id] or DynamicLight(id)
-	if not pac.dynamic_lights[id] then
-		pac.dynamic_lights[id] = self.Params
-	end
-	local params = self.Params
-	if params then
-		params.Pos = pos
-		
-		params.MinLight = self.Brightness
-		params.Size = self.Size
-		params.Style = self.Style
-		
-		params.r = self.Color.r
-		params.g = self.Color.g
-		params.b = self.Color.b		
-		
-		-- 100000000 constant is better than calling pac.RealTime
-		params.DieTime = 1000000000000 -- pac.RealTime
-	end
+	local light = self.light or DynamicLight(tonumber(self.UniqueID))
+	
+	light.Pos = pos
+	
+	light.MinLight = self.Brightness
+	light.Size = self.Size
+	light.Style = self.Style
+	
+	light.r = self.Color.r
+	light.g = self.Color.g
+	light.b = self.Color.b		
+	
+	-- 100000000 constant is better than calling pac.RealTime
+	light.DieTime = 1000000000000 -- pac.RealTime
+	
+	self.light = light
 end
 
 function PART:OnHide()
-	local p = self.Params 
-	if p then
-		p.DieTime = 0
-		p.Size = 0
-		p.MinLight = 0
-		p.Pos = Vector()
+	local light = self.light 
+	if light then
+		light.DieTime = 0
+		light.Size = 0
+		light.MinLight = 0
+		light.Pos = Vector()
 	end
 end
 
