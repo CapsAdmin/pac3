4e0dace4aaef5e7b977159b53412aac938bd856b
Suchipi Izumi <me@suchipi.com>
2014-05-13 17:03:04 -0600

    Use JSON format for balanims
    I didn't realize this existed until just now and this is a lot better.
    Also don't try to do stuff to nonexistent entities

--- lua/pac3/core/client/parts/balanim.lua
+++ lua/pac3/core/client/parts/balanim.lua
@@ -34,14 +34,9 @@ function PART:Initialize()
 end
 
 function PART:OnThink() 
-	local function LoadBalAnim(str)
-		local regstring="RegisterLuaAnimation("..addquotes(tostring(self:GetUniqueID()))..","..str..")"
-		if string.find(regstring,"function") == nil then
-			local registeranim = CompileString(regstring,"registeranim")
-			animregenv = {RegisterLuaAnimation = RegisterLuaAnimation,} --create an environment where it's only possible to register animations
-			setfenv(registeranim, animregenv) --force registeranim() to run in the limited environment
-			pcall(registeranim) --run registeranim in the environment
-		end
+	local function LoadBalAnim(str) --I didn't know we had this until now
+	    t = pcall(util.JSONToTable, str)
+		RegisterLuaAnimation(addquotes(tostring(self:GetUniqueID())),t)
 	end
 	--reregister animation when URL changes
 	if currenturl ~= self:GetURL() then
@@ -54,18 +49,24 @@ end
 function PART:OnShow(owner, pos, ang)
 	--play animation
 	local owner = self:GetOwner()
-	owner:SetLuaAnimation(tostring(self:GetUniqueID()))
+	if owner and owner:IsValid() then
+	  owner:SetLuaAnimation(tostring(self:GetUniqueID()))
+	end
 end
 
 function PART:OnHide()
 	--stop animation
 	local owner = self:GetOwner()
-	owner:StopLuaAnimation(tostring(self:GetUniqueID()))
+	if owner and owner:IsValid() then
+	  owner:StopLuaAnimation(tostring(self:GetUniqueID()))
+	end
 end
 
 function PART:OnRemove() 
 	local owner = self:GetOwner()
-	owner:SetLuaAnimation("BlankAnim") --play BlankAnim so they don't get stuck
+	if owner and owner:IsValid() then
+	  owner:SetLuaAnimation("BlankAnim") --play BlankAnim so they don't get stuck
+	end
 	UnRegisterLuaAnimation(tostring(self:GetUniqueID())) --unregister the anim
 end
 
