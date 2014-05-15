db85a45de841578bf4fa287267d55d14c9ff7792
Suchipi Izumi <me@suchipi.com>
2014-05-15 00:53:28 -0600

    Optimize URL checking, verify anim existence
    URL checking no longer happens every frame or with a global variable
    instead of a local one (oops).
    
    Code now verifies anims exist before doing anything with them to prevent
    errors.
    
    If an anim that should have been registered when the URL was set didn't
    get registered, then it will now be registered when the event tries to
    play that anim.

--- lua/pac3/core/client/parts/balanim.lua
+++ lua/pac3/core/client/parts/balanim.lua
@@ -28,45 +28,54 @@ function PART:GetNiceName()
 end
 
 function PART:Initialize()
-	currentpath = ""
+--	currentpath = ""
 end
 
 function PART:OnThink() 
+-- nothing to do here
+end
+
+function PART:SetURL(url)
+	self.URL = url
 	local function LoadBalAnim(str)
 	    local thistable = util.JSONToTable(str)
 		RegisterLuaAnimation("pac_"..tostring(self:GetUniqueID()),thistable)
 	end
-	if currentpath ~= self:GetURL() then --reregister animation when input url changes
-		local var = self:GetURL()
-		if var and var:find("http") then
-			http.Fetch(self:GetURL(), LoadBalAnim)
-			currentpath = self:GetURL()
-		end
+	if url and url:find("http") then
+		http.Fetch(self:GetURL(), LoadBalAnim)
 	end
 end
 
 function PART:OnShow(owner, pos, ang)
 	--play animation
 	local owner = self:GetOwner()
-	if owner and owner:IsValid() then
-	  owner:SetLuaAnimation("pac_"..tostring(self:GetUniqueID()))
+	local Animations = GetLuaAnimations()
+	if not Animations["pac_"..tostring(self:GetUniqueID())] then
+		self:SetURL(self:GetURL()) --I don't like this but it isn't /bad/ per se
+	end
+	if owner and owner:IsValid() and Animations["pac_"..tostring(self:GetUniqueID())] then
+		owner:SetLuaAnimation("pac_"..tostring(self:GetUniqueID()))
 	end
 end
 
 function PART:OnHide()
 	--stop animation
 	local owner = self:GetOwner()
-	if owner and owner:IsValid() then
-	  owner:StopLuaAnimation("pac_"..tostring(self:GetUniqueID()))
+	local Animations = GetLuaAnimations()
+	if owner and owner:IsValid() and Animations["pac_"..tostring(self:GetUniqueID())] then
+		owner:StopLuaAnimation("pac_"..tostring(self:GetUniqueID()))
 	end
 end
 
 function PART:OnRemove() 
 	local owner = self:GetOwner()
+	local Animations = GetLuaAnimations()
 	if owner and owner:IsValid() then
-	  owner:SetLuaAnimation("BlankAnim") --play BlankAnim so they don't get stuck
+		owner:SetLuaAnimation("BlankAnim") --play BlankAnim so they don't get stuck
 	end
-	UnRegisterLuaAnimation("pac_"..tostring(self:GetUniqueID())) --unregister the anim
+	if Animations["pac_"..tostring(self:GetUniqueID())] then
+		UnRegisterLuaAnimation("pac_"..tostring(self:GetUniqueID())) --unregister the anim
+	end
 end
 
 pac.RegisterPart(PART)
