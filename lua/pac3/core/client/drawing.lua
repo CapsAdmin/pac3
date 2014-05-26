887d494755904688642b37d2e54921574807d65e
Suchipi Izumi <me@suchipi.com>
2014-05-26 02:42:29 -0600

    let's try that again
    anyone going to play silly buggers in this room I'll boot you

--- lua/pac3/core/client/drawing.lua
+++ lua/pac3/core/client/drawing.lua
@@ -303,7 +303,7 @@ function pac.IgnorePlayer(ply)
 end
 
 function pac.UnIgnorePlayer(ply)
-	list.Set("pac_ignored_players",ply:Nick(),0)
+	list.Set("pac_ignored_players",ply:Nick(),nil)
 	pac.ShowEntityParts(ply)
 end
 
@@ -421,7 +421,7 @@ function pac.PostDrawOpaqueRenderables(bool1, bool2, ...)
 				ent.IsPACWorldEntity or
 				(ent == pac.LocalPlayer and ent:ShouldDrawLocalPlayer() or (ent.pac_camera and ent.pac_camera:IsValid())) or
 				
-				ent ~= pac.LocalPlayer and --list.Get("pac_ignored_players")[ent:Nick()] ~= 1 and list.Get("pac_ignored_players")[ent:CPPIGetOwner():Nick()] ~= 1 and
+				ent ~= pac.LocalPlayer and not (list.Get("pac_ignored_players")[ent:Nick()]) and not (list.Get("pac_ignored_players")[ent:CPPIGetOwner():Nick()]) and
 				(					
 					((util_PixelVisible(ent:EyePos(), radius, ent.pac_pixvis) ~= 0 or fovoverride ~= 0) or (dst < radius * 1.25)) and 
 					(
