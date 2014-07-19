2883916408d8d197d48d79d2eee7fd2abcbbf4f1
Suchipi Izumi <me@suchipi.com>
2014-07-19 00:15:52 -0600

    Update animation editor
    This commit makes several changes:
    
    -Remove console command "pac_in_editor" and use the net library to
    transmit this information instead (this is used for the overhead "In
    Editor" text).
    
    -Replicate this behavior within the Animation Editor.
    
    -Add a "Parsed Skeleton" option to the animation editor that allows
    users to use any bone structure their current player entity has.
    
    -Disallow creating an animation without a name, which would cause errors
    later on.
    
    -Don't display boneanimlib animations that were created by the
    custom_animation pac part
    
    -When editing a sequence, t-pose the player so the user has a proper
    frame of reference
    
    -Automatically save backups of the current animation when the editor is
    closed
    
    -Allow any user to use the animation editor
    
    -Preserve frame length when cloning a keyframe
    
    -Various bugfixes

--- lua/pac3/core/client/netmessages.lua
+++ lua/pac3/core/client/netmessages.lua
@@ -3,3 +3,31 @@ net.Receive( "pac.net.TogglePartDrawing", function( length, client )
 		b = (net.ReadBit() == 1)
 		if ent:IsValid() then pac.TogglePartDrawing(ent, b) end
 end )
+
+function pac.SetInPAC3Editor(b)
+	net.Start("pac.net.InPAC3Editor.ClientNotify")
+	net.WriteBit(b)
+	net.SendToServer()
+end
+
+net.Receive( "pac.net.InPAC3Editor", function( length, client )
+    ent = net.ReadEntity()
+	b = (net.ReadBit() == 1)
+	if ent:IsValid() then 
+		ent.InPAC3Editor = b
+	end
+end )
+
+function pac.SetInAnimEditor(b)
+	net.Start("pac.net.InAnimEditor.ClientNotify")
+	net.WriteBit(b)
+	net.SendToServer()
+end
+
+net.Receive( "pac.net.InAnimEditor", function( length, client )
+    ent = net.ReadEntity()
+	b = (net.ReadBit() == 1)
+	if ent:IsValid() then 
+		ent.InAnimEditor = b
+	end
+end )
