6229f4c52c3874a56cab583b5b10fd8d9bfce8e0
notcake <cakenotfound@gmail.com>
2014-10-26 23:17:54 +0000

    urlobj : untangling coroutine logic from obj parser

--- lua/pac3/core/client/libraries/urlobj.lua
+++ lua/pac3/core/client/libraries/urlobj.lua
@@ -1,10 +1,11 @@
-local urlobj = pac.urlobj or {}
-pac.urlobj = urlobj
+pac.urlobj = pac.urlobj or {}
+local urlobj = pac.urlobj
 
-urlobj.Queue = {}
-urlobj.Cache = {}
+urlobj.Queue      = {}
 urlobj.QueueCount = 0
-urlobj.CacheCount = 0
+
+urlobj.Cache      = urlobj.Cache      or {}
+urlobj.CacheCount = urlobj.CacheCount or 0
 
 concommand.Add("pac_urlobj_clear_cache",
 	function ()
@@ -84,136 +85,145 @@ end
 local table_insert = table.insert
 local tonumber = tonumber
 
-local nextParsingHookId = 0 
-
-function urlobj.ParseObj(data, generateNormals, callback, statusCallback)
-	statusCallback = statusCallback or function (finished, statusMessage) end
+function urlobj.ParseObj(data, generateNormals)
+	local coroutine_yield = coroutine.running () and coroutine.yield or function () end
 	
-	local co = coroutine.create(function()
-		debug.sethook()
+	local positions = {}
+	local texcoords = {}
+	local normals = {}
+	
+	local output = {}
+	
+	local lines = {}
+	
+	local i = 1
+	for line in data:gmatch("(.-)\n") do
+		local parts = line:gsub("%s+", " "):Trim():Split(" ")
 
-		local positions = {}
-		local texcoords = {}
-		local normals = {}
-		
-		local output = {}
-		
-		local lines = {}
-		
-		local i = 1
-		for line in data:gmatch("(.-)\n") do
-			local parts = line:gsub("%s+", " "):Trim():Split(" ")
+		table_insert(lines, parts)
+		coroutine.yield(false, "inserting lines", i)
+		i = i + 1
+	end
 
-			table_insert(lines, parts)
-			coroutine.yield(false, "inserting lines", i)
-			i = i + 1
+	local vert_count = #lines
+
+	for i, parts in pairs(lines) do		
+		if parts[1] == "v" and #parts >= 4 then
+			table_insert(positions, Vector(tonumber(parts[2]), tonumber(parts[3]), tonumber(parts[4])))
+		elseif parts[1] == "vt" and #parts >= 3 then
+			table_insert(texcoords, {u = tonumber(parts[2])%1, v = tonumber(1-parts[3])%1})
+		elseif not generateNormals and parts[1] == "vn" and #parts >= 4 then
+			table_insert(normals, Vector(tonumber(parts[2]), tonumber(parts[3]), tonumber(parts[4])):GetNormalized())
 		end
-	
-		local vert_count = #lines
-	
-		for i, parts in pairs(lines) do		
-			if parts[1] == "v" and #parts >= 4 then
-				table_insert(positions, Vector(tonumber(parts[2]), tonumber(parts[3]), tonumber(parts[4])))
-			elseif parts[1] == "vt" and #parts >= 3 then
-				table_insert(texcoords, {u = tonumber(parts[2])%1, v = tonumber(1-parts[3])%1})
-			elseif not generateNormals and parts[1] == "vn" and #parts >= 4 then
-				table_insert(normals, Vector(tonumber(parts[2]), tonumber(parts[3]), tonumber(parts[4])):GetNormalized())
-			end
+		
+		coroutine.yield(false, "parsing lines", (i/vert_count))
+	end
 			
-			coroutine.yield(false, "parsing lines", (i/vert_count))
-		end
+	for i, parts in pairs(lines) do
+		if parts[1] == "f" and #parts > 3 then
+			local first, previous
+
+			for i = 2, #parts do
+				local current = parts[i]:Split("/")
+
+				if i == 2 then
+					first = current
+				end
 				
-		for i, parts in pairs(lines) do
-			if parts[1] == "f" and #parts > 3 then
-				local first, previous
+				if i >= 4 then
+					local v1, v2, v3 = {}, {}, {}
 
-				for i = 2, #parts do
-					local current = parts[i]:Split("/")
-
-					if i == 2 then
-						first = current
+					v1.pos_index = tonumber(first[1])
+					v2.pos_index = tonumber(current[1])
+					v3.pos_index = tonumber(previous[1])
+					
+					v1.pos = positions[tonumber(first[1])]
+					v2.pos = positions[tonumber(current[1])]
+					v3.pos = positions[tonumber(previous[1])]
+					
+					if #texcoords > 0 then
+						v1.u = texcoords[tonumber(first[2])].u
+						v1.v = texcoords[tonumber(first[2])].v
+						
+						v2.u = texcoords[tonumber(current[2])].u
+						v2.v = texcoords[tonumber(current[2])].v
+						
+						v3.u = texcoords[tonumber(previous[2])].u
+						v3.v = texcoords[tonumber(previous[2])].v
 					end
 					
-					if i >= 4 then
-						local v1, v2, v3 = {}, {}, {}
-
-						v1.pos_index = tonumber(first[1])
-						v2.pos_index = tonumber(current[1])
-						v3.pos_index = tonumber(previous[1])
-						
-						v1.pos = positions[tonumber(first[1])]
-						v2.pos = positions[tonumber(current[1])]
-						v3.pos = positions[tonumber(previous[1])]
-						
-						if #texcoords > 0 then
-							v1.u = texcoords[tonumber(first[2])].u
-							v1.v = texcoords[tonumber(first[2])].v
-							
-							v2.u = texcoords[tonumber(current[2])].u
-							v2.v = texcoords[tonumber(current[2])].v
-							
-							v3.u = texcoords[tonumber(previous[2])].u
-							v3.v = texcoords[tonumber(previous[2])].v
-						end
-						
-						if #normals > 0 then
-							v1.normal = normals[tonumber(first[3])]
-							v2.normal = normals[tonumber(current[3])]
-							v3.normal = normals[tonumber(previous[3])]
-						end				
-						
-						table_insert(output, v1)
-						table_insert(output, v2)
-						table_insert(output, v3)
-					end
-
-					previous = current
+					if #normals > 0 then
+						v1.normal = normals[tonumber(first[3])]
+						v2.normal = normals[tonumber(current[3])]
+						v3.normal = normals[tonumber(previous[3])]
+					end				
+					
+					table_insert(output, v1)
+					table_insert(output, v2)
+					table_insert(output, v3)
 				end
-			end
-			
-			coroutine.yield(false, "solving indices", i/vert_count)
-		end
-		
-		if generateNormals then
-			local vertex_normals = {}
-			local count = #output/3
-			for i = 1, count do
-				local a, b, c = output[1+(i-1)*3+0], output[1+(i-1)*3+1], output[1+(i-1)*3+2] 
-				local normal = (c.pos - a.pos):Cross(b.pos - a.pos):GetNormalized()
 
-				vertex_normals[a.pos_index] = vertex_normals[a.pos_index] or Vector()
-				vertex_normals[a.pos_index] = (vertex_normals[a.pos_index] + normal)
-
-				vertex_normals[b.pos_index] = vertex_normals[b.pos_index] or Vector()
-				vertex_normals[b.pos_index] = (vertex_normals[b.pos_index] + normal)
-
-				vertex_normals[c.pos_index] = vertex_normals[c.pos_index] or Vector()
-				vertex_normals[c.pos_index] = (vertex_normals[c.pos_index] + normal)
-				coroutine.yield(false, "generating normals", i/count)
-			end
-			
-			local default_normal = Vector(0, 0, -1)
-
-			local count = #output
-			for i = 1, count do
-				local n = vertex_normals[output[i].pos_index] or default_normal
-				n:Normalize()
-				normals[i] = n
-				output[i].normal = n
-				coroutine.yield(false, "smoothing normals", i/count)
+				previous = current
 			end
 		end
 		
-		callback(output)
-		coroutine.yield(true)
-	end)
+		coroutine.yield(false, "solving indices", i/vert_count)
+	end
 	
+	if generateNormals then
+		local vertex_normals = {}
+		local count = #output/3
+		for i = 1, count do
+			local a, b, c = output[1+(i-1)*3+0], output[1+(i-1)*3+1], output[1+(i-1)*3+2] 
+			local normal = (c.pos - a.pos):Cross(b.pos - a.pos):GetNormalized()
+
+			vertex_normals[a.pos_index] = vertex_normals[a.pos_index] or Vector()
+			vertex_normals[a.pos_index] = (vertex_normals[a.pos_index] + normal)
+
+			vertex_normals[b.pos_index] = vertex_normals[b.pos_index] or Vector()
+			vertex_normals[b.pos_index] = (vertex_normals[b.pos_index] + normal)
+
+			vertex_normals[c.pos_index] = vertex_normals[c.pos_index] or Vector()
+			vertex_normals[c.pos_index] = (vertex_normals[c.pos_index] + normal)
+			coroutine.yield(false, "generating normals", i/count)
+		end
+		
+		local default_normal = Vector(0, 0, -1)
+
+		local count = #output
+		for i = 1, count do
+			local n = vertex_normals[output[i].pos_index] or default_normal
+			n:Normalize()
+			normals[i] = n
+			output[i].normal = n
+			coroutine.yield(false, "smoothing normals", i/count)
+		end
+	end
+end
+
+local nextParsingHookId = 0 
+
+function urlobj.CreateObj(obj_str, generateNormals, statusCallback)
+	statusCallback = statusCallback or function (finished, statusMessage) end
+	
+	local mesh = Mesh()
+	
+	local co = coroutine.create (
+		function ()
+			local meshData = urlobj.ParseObj(data, generateNormals)
+			mesh:BuildFromTriangles (meshData)
+			
+			coroutine.yield (true)
+		end
+	)
+	
+	-- Coroutine runner
 	hook.Add("Think", "pac_parse_obj_" .. nextParsingHookId,
 		function()
 			local t0 = SysTime ()
 			while SysTime () - t0 < 0.002 do
-				local dead, finished, statusMessage, msg = coroutine.resume(co)
-				if finished then
+				local success, finished, statusMessage, msg = coroutine.resume(co)
+				if not success or finished then
 					if statusCallback then statusCallback(true, "finished") end
 					hook.Remove("Think", "pac_parse_obj_" .. nextParsingHookId)
 				else
@@ -230,19 +240,6 @@ function urlobj.ParseObj(data, generateNormals, callback, statusCallback)
 	)
 	
 	nextParsingHookId = nextParsingHookId + 1
-end
-
-function urlobj.CreateObj(obj_str, generateNormals, statusCallback)
-	statusCallback = statusCallback or function (finished, statusMessage) end
-	
-	local mesh = Mesh()
-	
-	urlobj.ParseObj(obj_str, generateNormals,
-		function(data)
-			mesh:BuildFromTriangles(data)
-		end,
-		statusCallback
-	)
 	
 	return { mesh }
 end
