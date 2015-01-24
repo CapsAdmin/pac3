9f475b92402ac672bfb4fb160a343272649ab645
Suchipi Izumi <me@suchipi.com>
2015-01-24 09:02:06 +0000

    allow entering flex name or idyou can now put a string flex name (smile) or the id of a flex (42) into the flex field and the system will handle it properly

--- lua/pac3/core/client/parts/flex.lua
+++ lua/pac3/core/client/parts/flex.lua
@@ -4,7 +4,7 @@ PART.ClassName = "flex"
 PART.NonPhysical = true
 
 pac.StartStorableVars()
-	pac.GetSet(PART, "Flex", -1)
+	pac.GetSet(PART, "Flex", "")
 	pac.GetSet(PART, "Weight", 0)
 	pac.GetSet(PART, "RootOwner", true)
 	pac.GetSet(PART, "DefaultOnHide", true)
@@ -17,12 +17,21 @@ end
 function PART:GetFlexName()
 	local name
 	local owner = self:GetOwner(self.RootOwner)
-	local flex = self:GetFlex()
+	local flex = self:ResolveFlex(self:GetFlex())
 	if flex ~= -1 and (owner:GetFlexNum() > 0) then name = owner:GetFlexName(flex) end
 	return name
 end
 
+function PART:Initialize()
+	local owner = self:GetOwner(self.RootOwner)
+	if not owner:IsValid() then return end
+	local t = {}
+	for i=1,owner:GetFlexNum() do t[owner:GetFlexName(i)] = i end
+	self.FlexList = t
+end
+
 function PART:UpdateFlex(flex,weight)
+	flex = self:ResolveFlex(flex)
 	local owner = self:GetOwner(self.RootOwner)
 	if not owner.GetFlexNum then return end
 	if not (owner:GetFlexNum() > 0) then return end
@@ -34,6 +43,19 @@ function PART:UpdateFlex(flex,weight)
 	if flex < 0 then return end
 	
 	owner:SetFlexWeight(flex,weight)
+end
+
+function PART:ResolveFlex(str)
+		
+	local internal_flex = tonumber(str)
+	
+	if internal_flex == nil then --they entered a string
+		if str == "" then internal_flex = -1
+		else internal_flex = self.FlexList[str] or -1
+		end
+	end
+	
+	return internal_flex
 end
 
 function PART:SetFlex(flex)
