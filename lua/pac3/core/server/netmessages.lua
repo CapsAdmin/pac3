24edb974a8aa5d57c83134bc54e332a20806cf54
Suchipi Izumi <me@suchipi.com>
2015-04-17 16:04:43 -0600

    change touch flexes to default to current weight instead of 0

--- lua/pac3/core/server/netmessages.lua
+++ lua/pac3/core/server/netmessages.lua
@@ -43,5 +43,6 @@ util.AddNetworkString("pac.net.TouchFlexes.ClientNotify")
 net.Receive( "pac.net.TouchFlexes.ClientNotify", function( length, client )
 	local index = net.ReadInt(13)
 	local ent = Entity(index)
-	if ent and ent:IsValid() and ent.GetFlexNum and ent:GetFlexNum() > 0 then ent:SetFlexWeight(1,0) end
+	local target = ent:GetFlexWeight(1) or 0
+	if ent and ent:IsValid() and ent.GetFlexNum and ent:GetFlexNum() > 0 then ent:SetFlexWeight(1,target) end
 end )
