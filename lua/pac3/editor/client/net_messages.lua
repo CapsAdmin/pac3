68f0fdfcf8a395b1fdaf7cf573af3be81dba624f
CapsAdmin <eliashogstvedt@gmail.com>
2015-04-20 16:32:49 +0200

    removed player collide from entity moved editor net messages from core/* to editor/*

---
+++ lua/pac3/editor/client/net_messages.lua
@@ -0,0 +1,27 @@
+function pace.SetInPAC3Editor(b)
+	net.Start("pac.net.InPAC3Editor.ClientNotify")
+	net.WriteBit(b)
+	net.SendToServer()
+end
+
+net.Receive( "pac.net.InPAC3Editor", function( length, client )
+    ent = net.ReadEntity()
+	b = (net.ReadBit() == 1)
+	if ent:IsValid() then 
+		ent.InPAC3Editor = b
+	end
+end )
+
+function pace.SetInAnimEditor(b)
+	net.Start("pac.net.InAnimEditor.ClientNotify")
+	net.WriteBit(b)
+	net.SendToServer()
+end
+
+net.Receive( "pac.net.InAnimEditor", function( length, client )
+    ent = net.ReadEntity()
+	b = (net.ReadBit() == 1)
+	if ent:IsValid() then 
+		ent.InAnimEditor = b
+	end
+end )
