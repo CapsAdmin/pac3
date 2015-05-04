edf572ef5d9dfd440efc0f1c0597d3d51369aa5e
CapsAdmin <eliashogstvedt@gmail.com>
2015-05-04 10:17:37 +0200

    cleanup in model modifier code default modifier cvars off unless running sandbox (solves issue #155)

--- lua/pac3/core/shared/modifiers/model.lua
+++ lua/pac3/core/shared/modifiers/model.lua
@@ -6,16 +6,27 @@ end
 if SERVER then
 
 	function pac.SetPlayerModel(ply, model)
-		if Clockwork then return end -- Clockwork fix
 		model = player_manager.AllValidModels()[model] or model
 		
-		if not util.IsValidModel(model) then
+		if not util.IsValidModel(model) and ply:GetInfo("cl_playermodel") then
 			model = player_manager.TranslatePlayerModel(ply:GetInfo("cl_playermodel"))
 		end
 		
 		ply:SetModel(model)
 		
 		ply.pac_last_modifier_model = model:lower()
+		
+		hook.Add("Think", "pac_setmodel", function(ply)
+			if GetConVarNumber("pac_modifier_model") == 0 then
+				hook.Remove("Think", "pac_setmodel")
+				return
+			end
+			for key, ply in pairs(player.GetAll()) do
+				if ply.pac_last_modifier_model and ply:GetModel():lower() ~= ply.pac_last_modifier_model then
+					ply:SetModel(ply.pac_last_modifier_model)
+				end
+			end
+		end)
 	end
 	
 	pac.AddServerModifier("model", function(data, owner) 
@@ -41,17 +52,8 @@ if SERVER then
 	end)
 			
 	concommand.Add("pac_setmodel", function(ply, _, args)
-		if GetConVarNumber("pac_modifier_model") ~= 0 and ply:GetInfo("pac_modifier_model") ~= 0 and !Clockwork then
+		if GetConVarNumber("pac_modifier_model") ~= 0 and ply:GetInfo("pac_modifier_model") ~= 0 then
 			pac.SetPlayerModel(ply, args[1])	
-		end
-	end)
-	
-	hook.Add("Think", "pac_setmodel", function(ply)	
-		if Clockwork then hook.Remove("Think", "pac_setmodel") return end
-		for key, ply in pairs(player.GetAll()) do
-			if ply.pac_last_modifier_model and ply:GetModel():lower() ~= ply.pac_last_modifier_model then
-				ply:SetModel(ply.pac_last_modifier_model)
-			end
 		end
 	end)
 end
