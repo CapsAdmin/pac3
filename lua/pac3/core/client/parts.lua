0247b23c885a2958d5fa1c003a73204df09ea012
CapsAdmin <eliashogstvedt@gmail.com>
2015-05-21 17:58:00 +0200

    delay pac_OnPartCreated event to ensure parent is set faster refresh rate for editor tree

--- lua/pac3/core/client/parts.lua
+++ lua/pac3/core/client/parts.lua
@@ -24,7 +24,7 @@ local function merge_storable(tbl, base)
 	end
 end
 
-function pac.CreatePart(name, owner, skip_hook)
+function pac.CreatePart(name, owner)
 	owner = owner or pac.LocalPlayer
 	
 	if not name then
@@ -87,7 +87,11 @@ function pac.CreatePart(name, owner, skip_hook)
 		
 	pac.dprint("creating %s part owned by %s", part.ClassName, tostring(owner))
 	
-	pac.CallHook("OnPartCreated", part, owner == pac.LocalPlayer)
+	timer.Simple(0.1, function()
+		if part:IsValid() then
+			pac.CallHook("OnPartCreated", part, owner == pac.LocalPlayer)
+		end
+	end)
 	
 	return part
 end
