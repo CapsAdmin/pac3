0bb3fd1d7c71a355f67233e9ee79a0deac3667ee
Valentin Funk <funk.valentin@gmail.com>
2016-05-28 11:42:28 +0200

    Disable flashlight in 2d context to avoid crashes

--- lua/pac3/core/client/drawing.lua
+++ lua/pac3/core/client/drawing.lua
@@ -13,20 +13,20 @@ pac.RealTime = 0
 pac.FrameNumber = 0
 
 local sort = function(a, b)
-	if a and b then 
+	if a and b then
 		if a.DrawOrder and b.DrawOrder then
 			return a.DrawOrder < b.DrawOrder
 		end
-		
+
 		if a.part and b.part and a.part.DrawOrder and b.part.DrawOrder then
 			return a.part.DrawOrder < b.part.DrawOrder
 		end
 	end
 end
-	
+
 local function think(part)
 	if part.ThinkTime == 0 then
-		if part.last_think ~= pac.FrameNumber then 
+		if part.last_think ~= pac.FrameNumber then
 			part:Think()
 			part.last_think = pac.FrameNumber
 		end
@@ -34,7 +34,7 @@ local function think(part)
 		part:Think()
 		part.last_think = pac.RealTime + (part.ThinkTime or 0.1)
 	end
-	
+
 	for _, part in pairs(part.Children) do
 		think(part)
 	end
@@ -42,7 +42,7 @@ end
 
 local function buildbones(part)
 	part:BuildBonePositions()
-	
+
 	for _, part in pairs(part.Children) do
 		buildbones(part)
 	end
@@ -66,21 +66,21 @@ local GARBAGE = math.huge
 pac.profile_info = {}
 pac.profile = true
 
-function pac.GetProfilingData(ent)	
+function pac.GetProfilingData(ent)
 	local data = pac.profile_info[ent:EntIndex()]
-	
+
 	if data then
 		local out = {events = {}}
 		out.times_rendered = data.times_ran
-		
-		
+
+
 		for type, data in pairs(data.types) do
 			out.events[type] = {
 				average_garbage = data.total_garbage / out.times_rendered,
 				average_ms = data.total_render_time / out.times_rendered,
 			}
 		end
-		
+
 		return out
 	end
 end
@@ -93,8 +93,8 @@ local function hide_parts(ent)
 			part:SetKeyValueRecursive("shown_from_rendering", false)
 			part:SetKeyValueRecursive("draw_hidden", true)
 		end
-		
-		pac.ResetBones(ent)		
+
+		pac.ResetBones(ent)
 		ent.pac_drawing = false
 	end
 end
@@ -107,7 +107,7 @@ local function show_parts(ent)
 			part:SetKeyValueRecursive("shown_from_rendering", true)
 			part:SetKeyValueRecursive("draw_hidden", false)
 		end
-		
+
 		pac.ResetBones(ent)
 		ent.pac_drawing = true
 	end
@@ -130,28 +130,28 @@ pac.ShowEntityParts = show_parts
 pac.TogglePartDrawing = toggle_drawing_parts
 
 local function render_override(ent, type, draw_only)
-	
+
 	if pac.profile then
 		TIME = util_TimerCycle()
 		GARBAGE = collectgarbage("count")
 	end
-	
+
 	if max_render_time > 0 and ent ~= pac.LocalPlayer then
 		render_time = SysTime()
-		
+
 		if ent.pac_render_time_stop and ent.pac_render_time_stop > render_time then
 			return
 		end
 	end
 
-	
+
 	if not ent.pac_parts then
 		pac.UnhookEntityRender(ent)
 	else
 		if not draw_only then
-		
+
 			pac.ResetBones(ent)
-			
+
 			-- bones MUST be setup before drawing or else unexpected/random results might happen
 			for key, part in pairs(ent.pac_parts) do
 				if part:IsValid() then
@@ -163,18 +163,18 @@ local function render_override(ent, type, draw_only)
 				end
 			end
 		end
-		
+
 		for key, part in pairs(ent.pac_parts) do
-			if part:IsValid() then		
+			if part:IsValid() then
 				if not part:HasParent() then
-					if not draw_only then 
-						think(part) 
-					end			
+					if not draw_only then
+						think(part)
+					end
 
 					if part.OwnerName == "viewmodel" and type ~= "viewmodel" then
 						continue
 					end
-					
+
 					if part.OwnerName ~= "viewmodel" and type == "viewmodel" then
 						continue
 					end
@@ -190,38 +190,38 @@ local function render_override(ent, type, draw_only)
 	if pac.profile then
 		TIME = util_TimerCycle()
 		GARBAGE = collectgarbage("count") - GARBAGE
-		
+
 		local id = ent:EntIndex()
 		pac.profile_info[id] = pac.profile_info[id] or {types = {}, times_ran = 0}
-		pac.profile_info[id].times_ran = pac.profile_info[id].times_ran + 1		
+		pac.profile_info[id].times_ran = pac.profile_info[id].times_ran + 1
 
 		pac.profile_info[id].types[type] = pac.profile_info[id].types[type] or {}
-		
+
 		local data = pac.profile_info[id].types[type]
-		
+
 		data.total_render_time = (data.total_render_time or 0) + TIME
 		data.total_garbage = (data.total_garbage or 0) + GARBAGE
-	end	
-		
-	if max_render_time > 0 and ent ~= pac.LocalPlayer then	
+	end
+
+	if max_render_time > 0 and ent ~= pac.LocalPlayer then
 		ent.pac_render_times = ent.pac_render_times or {}
-		
+
 		local last = ent.pac_render_times[type] or 0
-		
+
 		render_time = (SysTime() - render_time) * 1000
 		last = last + ((render_time - last) * FrameTime())
 		ent.pac_render_times[type] = last
-		
+
 		if last > max_render_time then
 			ent.pac_render_time_stop = SysTime() + 2 + (math.random() * 2)
-			
+
 			hide_parts(ent)
 		end
 	end
-	
+
 	render_SetColorModulation(1,1,1)
 	render_SetBlend(1)
-	
+
 	render_MaterialOverride()
 	render_ModelMaterialOverride()
 end
@@ -233,13 +233,13 @@ function pac.RenderOverride(ent, type, draw_only)
 	if not ok then
 		print("pac3 failed to render ", tostring(ent), ":")
 		print(err)
-		
+
 		if ent == pac.LocalPlayer then
 			chat.AddText("your pac3 outfit failed to render!")
 			chat.AddText(err)
 			chat.AddText("hiding your outfit to prevent further errors")
 		end
-		
+
 		ent.pac_error = err
 		table.insert(pac.Errors, err)
 		hide_parts(ent)
@@ -250,20 +250,20 @@ end
 
 pac.firstperson_parts = pac.firstperson_parts or {}
 
-function pac.HookEntityRender(ent, part)		
+function pac.HookEntityRender(ent, part)
 	if not ent.pac_parts then
 		ent.pac_parts = {}
 	end
-	
+
 	if ent.pac_parts[part] then
-		return 
+		return
 	end
-	
+
 	pac.dprint("hooking render on %s to draw part %s", tostring(ent), tostring(part))
-	
+
 	ent.pac_parts[part] = part
-	pac.drawn_entities[ent:EntIndex()] = ent	
-	pac.profile_info[ent:EntIndex()] = nil	
+	pac.drawn_entities[ent:EntIndex()] = ent
+	pac.profile_info[ent:EntIndex()] = nil
 end
 
 function pac.UnhookEntityRender(ent, part)
@@ -271,12 +271,12 @@ function pac.UnhookEntityRender(ent, part)
 	if part and ent.pac_parts then
 		ent.pac_parts[part] = nil
 	end
-	
+
 	if ent.pac_parts and not next(ent.pac_parts) then
 		pac.drawn_entities[ent:EntIndex()] = nil
 		ent.pac_parts = nil
 	end
-	
+
 	pac.profile_info[ent:EntIndex()] = nil
 end
 
@@ -325,11 +325,16 @@ function pac.ForceRendering(b)
 		force_rendering = b
 end
 
+-- disable pop/push flashlight modes (used for stability in 2D context)
+function pac.FlashlightDisable(b)
+	pac.flashlight_disabled = b
+end
+
 -- skybox hack --
 local in_skybox = false
 
 hook.Add("PreDrawSkyBox","pac",function()
-	if in_skybox==true then 
+	if in_skybox==true then
 		hook.Remove("PreDrawSkyBox","pac")
 		in_skybox = false
 		error"in_skybox was never disabled"
@@ -340,38 +345,38 @@ hook.Add("PostDrawSkyBox","pac",function()
 	in_skybox = false
 end)
 -----------------
-	
+
 local function setup_suppress()
 	local last_framenumber = 0
 	local current_frame = 0
 	local current_frame_count = 0
-	
+
 	return function()
 		if force_rendering then
 			return false
 		end
-		
+
 		--if in_skybox then return end
-		
-		if skip_rendering then 
+
+		if skip_rendering then
 			return true
 		end
-		
+
 		if cvar_framesuppress:GetBool() then
 			local frame_number = FrameNumber()
-			
+
 			if frame_number == last_framenumber then
 				current_frame = current_frame + 1
 			else
 				last_framenumber = frame_number
-							
+
 				if current_frame_count ~= current_frame then
 					current_frame_count = current_frame
 				end
-				
+
 				current_frame = 1
 			end
-					
+
 			return current_frame < current_frame_count
 		end
 	end
@@ -388,12 +393,12 @@ local dst = 0
 --local garbage = 0
 
 local should_suppress = setup_suppress()
-function pac.PostDrawOpaqueRenderables(drawdepth,drawing_skybox)	
+function pac.PostDrawOpaqueRenderables(drawdepth,drawing_skybox)
 	if in_skybox or should_suppress() then return end
-	
+
 	--garbage = collectgarbage("count")
-	
-	-- commonly used variables		
+
+	-- commonly used variables
 	max_render_time = max_render_time_cvar:GetFloat()
 	pac.RealTime = RealTime()
 	pac.FrameNumber = FrameNumber()
@@ -402,29 +407,29 @@ function pac.PostDrawOpaqueRenderables(drawdepth,drawing_skybox)
 	fovoverride = cvar_fovoverride:GetInt()
 	sv_draw_dist = GetConVarNumber("pac_sv_draw_distance")
 	radius = 0
-	
+
 	if draw_dist == 0 then
 		draw_dist = 32768
 	end
-	
+
 	for key, ent in pairs(pac.drawn_entities) do
 		if ent:IsValid() then
 			ent.pac_pixvis = ent.pac_pixvis or util.GetPixelVisibleHandle()
 			dst = ent:EyePos():Distance(pac.EyePos)
 			radius = ent:BoundingRadius() * 3 * (ent:GetModelScale() or 1)
-			
+
 			if ent:IsPlayer() then
-				
+
 				if not ent:Alive() and GetConVarNumber("pac_sv_hide_outfit_on_death") == 1 then
 					hide_parts(ent)
 					continue
 				end
-				
+
 				if ent:GetNoDraw() then
 					hide_parts(ent)
 					continue
 				end
-			
+
 				-- :(
 				if ent.pac_death_hide_ragdoll then
 					local ply = ent
@@ -439,45 +444,45 @@ function pac.PostDrawOpaqueRenderables(drawdepth,drawing_skybox)
 							ent:SetParent(ply)
 							ent:AddEffects(EF_BONEMERGE)
 						end
-						
+
 						if ply.pac_draw_player_on_death then
 							ply:DrawModel()
 						end
 					end
 				end
-			
+
 				if radius < 32 then
 					radius = 128
 				end
 			end
-			
+
 			if not ent:IsPlayer() and not ent:IsNPC() then
 				radius = radius * 4
 			end
-			
-			if 	
+
+			if
 				draw_dist == -1 or
 				ent.IsPACWorldEntity or
 				(ent == pac.LocalPlayer and ent:ShouldDrawLocalPlayer() or (ent.pac_camera and ent.pac_camera:IsValid())) or
-				ent ~= pac.LocalPlayer and 
-				(					
-					((util_PixelVisible(ent:EyePos(), radius, ent.pac_pixvis) ~= 0 or fovoverride ~= 0) or (dst < radius * 1.25)) and 
+				ent ~= pac.LocalPlayer and
+				(
+					((util_PixelVisible(ent:EyePos(), radius, ent.pac_pixvis) ~= 0 or fovoverride ~= 0) or (dst < radius * 1.25)) and
 					(
 						(sv_draw_dist ~= 0 and (sv_draw_dist == -1 or dst < sv_draw_dist)) or
 						(ent.pac_draw_distance and (ent.pac_draw_distance <= 0 or ent.pac_draw_distance < dst)) or
 						(dst < draw_dist)
 					)
 				)
-			then 
+			then
 				ent.pac_model = ent:GetModel() -- used for cached functions
-					
+
 				show_parts(ent)
-				
+
 				pac.RenderOverride(ent, "opaque")
 			else
 				hide_parts(ent)
 			end
-		else	
+		else
 			pac.drawn_entities[key] = nil
 		end
 	end
@@ -500,20 +505,20 @@ function pac.PostDrawTranslucentRenderables(drawing_depth,drawing_skybox)
 end
 pac.AddHook("PostDrawTranslucentRenderables")
 
-function pac.Think()	
+function pac.Think()
 	for key, ent in pairs(pac.drawn_entities) do
 		if ent:IsValid() then
 			if ent.pac_drawing and ent:IsPlayer() then
-			
+
 				ent.pac_traceres = util.QuickTrace(ent:EyePos(), ent:GetAimVector()*32000, {ent, ent:GetVehicle(), ent:GetOwner()})
 				ent.pac_hitpos = ent.pac_traceres.HitPos
-				
+
 			end
 		else
 			pac.drawn_entities[key] = nil
 		end
 	end
-	
+
 	if pac.next_frame_funcs then
 		for k, v in pairs(pac.next_frame_funcs) do
 			v()
