9ad6a5c831733d5100b0ef5967171227b17bb882
CapsAdmin <eliashogstvedt@gmail.com>
2016-12-20 22:25:14 +0100

    remove workaround

--- lua/autorun/client/render_override_workaround.lua
+++
@@ -1,9 +0,0 @@
-local skip = false 
-hook.Add("PrePlayerDraw", "renderoverride_workaround", function(p) 
-	if not skip and p.RenderOverride then 
-		skip = true 
-		p:RenderOverride() 
-		skip = false 
-		return true 
-	end
-end)
