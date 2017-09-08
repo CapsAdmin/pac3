ba8d6c954949a9dcd86800e98d70a6a9186c0191
DBot <roboderpy@gmail.com>
2017-09-08 11:14:36 +0700

    Moved footstep hack to net

--- lua/pac3/core/shared/footsteps_fix.lua
+++ lua/pac3/core/shared/footsteps_fix.lua
@@ -1,21 +1,23 @@
+
 if game.SinglePlayer() then
 	if SERVER then
+		util.AddNetworkString('pac_footstep')
 		hook.Add("PlayerFootstep", "pac_footstep_fix", function(ply, pos, _, snd, vol)
-			umsg.Start("pac_footstep")
-				umsg.Entity(ply)
-				umsg.Vector(pos)
-				umsg.String(snd)
-				umsg.Float(vol)
-			umsg.End()
+			net.Start("pac_footstep")
+				net.WriteEntity(ply)
+				net.WriteVector(pos)
+				net.WriteString(snd)
+				net.WriteFloat(vol)
+			net.Broadcast()
 		end)
 	end
 
 	if CLIENT then
-		usermessage.Hook("pac_footstep", function(umr)
-			local ply = umr:ReadEntity()
-			local pos = umr:ReadVector()
-			local snd = umr:ReadString()
-			local vol = umr:ReadFloat()
+		net.Receive("pac_footstep", function(len)
+			local ply = net.ReadEntity()
+			local pos = net.ReadVector()
+			local snd = net.ReadString()
+			local vol = net.ReadFloat()
 
 			if ply:IsValid() then
 				hook.Run("pac_PlayerFootstep", ply, pos, snd, vol)
