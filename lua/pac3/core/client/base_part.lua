c8d28b90fe71668d6c4973cb0d6ee97ebd7b781f
CapsAdmin <eliashogstvedt@gmail.com>
2017-09-20 14:54:25 +0200

    add ignorez and ways to set blend mode to all parts

--- lua/pac3/core/client/base_part.lua
+++ lua/pac3/core/client/base_part.lua
@@ -61,11 +61,58 @@ pac.StartStorableVars()
 
 	pac.SetPropertyGroup("appearance")
 		pac.GetSet(PART, "Translucent", false)
+		pac.GetSet(PART, "IgnoreZ", false)
+		pac.GetSet(PART, "BlendMode", "", {enums = {
+			none = "one;zero;one;zero",
+			alpha = "src_alpha;one_minus_src_alpha;one;one_minus_src_alpha",
+			multiplicative = "dst_color;zero;dst_color;zero",
+			premultiplied = "one;one_src_minus_alpha;one;one_src_minus_alpha",
+			additive = "src_alpha;one;src_alpha;one",
+		}})
 
 	pac.SetPropertyGroup("other")
 		pac.GetSet(PART, "DrawOrder", 0)
 
 pac.EndStorableVars()
+
+local blend_modes = {
+	zero = 0,
+	one = 1,
+	dst_color = 2,
+	one_minus_dst_color = 3,
+	src_alpha = 4,
+	one_minus_src_alpha = 5,
+	dst_alpha = 6,
+	one_minus_dst_alpha = 7,
+	src_alpha_saturate = 8,
+	src_color = 9,
+	one_minus_src_color = 10,
+}
+
+function PART:SetBlendMode(str)
+	str = str:lower():gsub("%s+", ""):gsub(",", ";"):gsub("blend_", "")
+
+	self.BlendMode = str
+
+	local tbl = str:Split(";")
+	local src_color
+	local dst_color
+
+	local src_alpha
+	local dst_alpha
+
+	if tbl[1] then src_color = blend_modes[tbl[1]] end
+	if tbl[2] then dst_color = blend_modes[tbl[2]] end
+
+	if tbl[3] then src_alpha = blend_modes[tbl[3]] end
+	if tbl[4] then dst_alpha = blend_modes[tbl[4]] end
+
+	if src_color and dst_color then
+		self.blend_override = {src_color, dst_color, src_alpha, dst_alpha, tbl[5]}
+	else
+		self.blend_override = nil
+	end
+end
 
 function PART:SetUniqueID(id)
 	if self.owner_id then
@@ -887,7 +934,38 @@ do -- drawing. this code is running every frame
 
 			if not self.HandleModifiersManually then self:ModifiersPreEvent('OnDraw', draw_type) end
 
+			if self.IgnoreZ then cam.IgnoreZ(true) end
+			if self.blend_override then
+				render.OverrideBlendFunc(true,
+					self.blend_override[1],
+					self.blend_override[2],
+					self.blend_override[3],
+					self.blend_override[4]
+				)
+
+				if self.blend_override[5] then
+					render.OverrideAlphaWriteEnable(true, self.blend_override[5] == "write_alpha")
+				end
+
+				if self.blend_override[6] then
+					render.OverrideColorWriteEnable(true, self.blend_override[6] == "write_color")
+				end
+			end
+
 			self:OnDraw(self:GetOwner(), pos, ang)
+
+			if self.blend_override then
+				render.OverrideBlendFunc(false)
+
+				if self.blend_override[5] then
+					render.OverrideAlphaWriteEnable(false)
+				end
+
+				if self.blend_override[6] then
+					render.OverrideColorWriteEnable(false)
+				end
+			end
+			if self.IgnoreZ then cam.IgnoreZ(false) end
 
 			if not self.HandleModifiersManually then self:ModifiersPostEvent('OnDraw', draw_type) end
 			self.selfDrawTime = SysTime() - sysTime
