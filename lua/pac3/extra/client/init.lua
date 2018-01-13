96ce116b85c3932de9b1346a2f33f0c65fc47dcf
DBotThePony <dbotthepony@yandex.ru>
2018-01-13 07:14:47 +0700

    Fix previous commit

--- lua/pac3/extra/client/init.lua
+++ lua/pac3/extra/client/init.lua
@@ -10,7 +10,7 @@ function pac.NetworkEntityCreated(ply)
 	if not ply:IsPlayer() then return end
 
 	if ply.pac_player_size then
-		pac.SetPlayerSize(ply,ply.pac_player_size,true)
+		pacx.SetPlayerSize(ply,ply.pac_player_size,true)
 	end
 
 end
@@ -21,13 +21,13 @@ function pac.NotifyShouldTransmit(ply, st)
 	if not st then return end
 
 	if ply:IsPlayer() and ply.pac_player_size then
-		pac.SetPlayerSize(ply, ply.pac_player_size, true)
+		pacx.SetPlayerSize(ply, ply.pac_player_size, true)
 
 		pac.RunNextFrame('update_entity_size_' .. ply:EntIndex(), function()
 			if not ply:IsValid() then return end
 
 			if ply.pac_player_size then
-				pac.SetPlayerSize(ply, ply.pac_player_size, true)
+				pacx.SetPlayerSize(ply, ply.pac_player_size, true)
 			end
 		end)
 	end
