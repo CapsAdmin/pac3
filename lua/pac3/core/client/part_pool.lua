83031be24158bb6de08f1d1dfe10d834b601b369
DBotThePony <dbotthepony@yandex.ru>
2018-01-25 20:28:17 +0700

    Revert "chore(part_pool): Remove unused function IsActuallyPlayer"
    This reverts commit 4355f5aaa3d97f6212097d558047063e5b6287cc.

--- lua/pac3/core/client/part_pool.lua
+++ lua/pac3/core/client/part_pool.lua
@@ -25,7 +25,11 @@ local IsValid = entMeta.IsValid
 local Alive = plyMeta.Alive
 
 local function IsActuallyValid(ent)
-	return IsValid(ent) and IsEntity(ent) and pcall(ent.GetPos, ent)
+	return IsEntity(ent) and pcall(ent.GetPos, ent)
+end
+
+local function IsActuallyPlayer(ent)
+	return IsEntity(ent) and pcall(ent.UniqueID, ent)
 end
 
 local ent_parts = {}
@@ -402,7 +406,7 @@ pac.AddHook("OnEntityCreated", function(ent)
 	if not IsActuallyValid(ent) then return end
 
 	local owner = ent:GetOwner()
-	if not IsActuallyValid(owner) then return end
+	if not IsValid(owner) then return end
 
 	if IsActuallyValid(owner) and (not owner:IsPlayer() or IsActuallyPlayer(owner)) then
 		for _, part in pairs(parts_from_ent(owner)) do
