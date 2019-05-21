0e5e01da78bf71609ad19a97d14e81797567ffa1
Python1320 <python1320+gh@gmail.com>
2019-05-21 19:55:27 +0300

    Break hiding meshes less

--- lua/pac3/core/client/parts/bone.lua
+++ lua/pac3/core/client/parts/bone.lua
@@ -120,6 +120,7 @@ local function manang(ent, id, ang, part)
 end
 
 local inf_scale = Vector(math.huge, math.huge, math.huge)
+local inf_scale_tempcrashfix = Vector(1,1,1)*0.001
 
 local function manscale(ent, id, scale, part)
 	if part.AlternativeBones then
@@ -257,15 +258,15 @@ function PART:OnBuildBonePositions()
 	local scale
 
 	-- Disable hide mesh functionality completely until the crash this causes, is fixed
-	if self.HideMesh and false then
-		scale = inf_scale
+	if self.HideMesh then
+		scale = inf_scale_tempcrashfix
 
 		if self.InvertHideMesh then
 			local count = owner:GetBoneCount()
 
 			for i = 0, count - 1 do
 				if i ~= self.BoneIndex then
-					manscale(owner, i, inf_scale, self)
+					manscale(owner, i, inf_scale_tempcrashfix, self)
 				end
 			end
 
