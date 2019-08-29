27bb5a405798c7dd278d682f0838c646b0df9ca9
DBotThePony <dbotthepony@yandex.ru>
2019-08-29 10:48:12 +0700

    Added PACChooseDeathRagdoll hook

--- lua/pac3/core/client/part_pool.lua
+++ lua/pac3/core/client/part_pool.lua
@@ -329,7 +329,7 @@ pac.AddHook("Think", "events", function()
 
 			if IsValid(rag) then
 				if ply.pac_ragdoll ~= rag then
-					ply.pac_ragdoll = rag
+					ply.pac_ragdoll = hook.Run("PACChooseDeathRagdoll", ply, rag) or rag
 
 					if ply.pac_death_physics_parts then
 						if ply.pac_physics_died then return end
