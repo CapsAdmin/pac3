8046e24b8bba6dbc5ff98d9501679b4a1d1e65a5
Techbot <techbot121@gmail.com>
2019-10-06 22:50:06 +0200

    ditto

--- lua/pac3/editor/client/wear.lua
+++ lua/pac3/editor/client/wear.lua
@@ -9,19 +9,19 @@ do -- to server
 		if pac_wear_friends_only:GetBool() then
 			for i, v in ipairs(player.GetAll()) do
 				if v:GetFriendStatus() == "friend" then
-					table.insert(filter, tostring(v:UniqueID()))
+					table.insert(filter, v:UniqueID())
 				end
 			end
 		elseif pac_wear_reverse:GetBool() then
 			for i, v in ipairs(player.GetAll()) do
 				if cookie.GetString('pac3_wear_block_' .. v:UniqueID(), '0') == '1' then
-					table.insert(filter, tostring(v:UniqueID()))
+					table.insert(filter, v:UniqueID())
 				end
 			end
 		else
 			for i, v in ipairs(player.GetAll()) do
 				if cookie.GetString('pac3_wear_block_' .. v:UniqueID(), '0') ~= '1' then
-					table.insert(filter, tostring(v:UniqueID()))
+					table.insert(filter, v:UniqueID())
 				end
 			end
 		end
