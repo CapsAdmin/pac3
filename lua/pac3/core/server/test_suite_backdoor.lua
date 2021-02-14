fedfef4ddaa34d5e0a119841a25a840e079ea0d5
CapsAdmin <eliashogstvedt@gmail.com>
2021-02-14 10:49:53 +0100

    improve comment

--- lua/pac3/core/server/test_suite_backdoor.lua
+++ lua/pac3/core/server/test_suite_backdoor.lua
@@ -1,10 +1,12 @@
 -- this is for the test suite, it is technically a lua run backdoor
--- it is used for things like getting what the actual model on the server for an entity is
+-- the test suite is a utility for the pac developers to ensure that pac works and don't break 
+-- after making changes to it
 
 util.AddNetworkString("pac3_test_sutie_backdoor_receive_results")
 util.AddNetworkString("pac3_test_sutie_backdoor")
 
 net.Receive("pac3_test_sutie_backdoor", function(len, ply)
+	-- need to be at least super admin
 	if not ply:IsSuperAdmin() then return end
 
 	local id = net.ReadString()
