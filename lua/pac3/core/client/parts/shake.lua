28a62524012edc61b35b97ff88c236e6d4e7e9ab
Yagira <yagiraa@gmail.com>
2021-12-24 02:22:00 +0100

    local vars + new scaling

--- lua/pac3/core/client/parts/shake.lua
+++ lua/pac3/core/client/parts/shake.lua
@@ -16,11 +16,11 @@ BUILDER:EndStorableVars()
 
 function PART:OnShow(from_rendering)
 	if not from_rendering then
-		position = self:GetDrawPosition()
-		eyedistance = position:Distance(pac.EyePos)
-		radius = math.Clamp(self.Radius, 0.0001, 500)
+		local position = self:GetDrawPosition()
+		local eyedistance = position:Distance(pac.EyePos)
+		local radius = math.Clamp(self.Radius, 0.0001, 500)
 		
-		amplitude = (self.Falloff and (1 - eyedistance / radius) * self.Amplitude) or (eyedistance < radius and self.Amplitude or 0)
+		local amplitude = (self.Falloff and (1 - (eyedistance / radius) ^ 2) * self.Amplitude) or (eyedistance < radius and self.Amplitude or 0)
 
 		util.ScreenShake(position, amplitude, self.Frequency, math.Clamp(self.Duration, 0.0001, 2), 0)
 	end
