25270d9acac7bfcc4f389eebc998c9a42794d086
Yagira <yagiraa@gmail.com>
2021-12-27 07:09:34 +0100

    scale shake amplitude with distance (#1136)
    * scale shake amplitude with distance
    
    * local vars + new scaling
    
    * Revert calculation
    
    Co-authored-by: Garrett Brown <grbrown93@sbcglobal.net>

--- lua/pac3/core/client/parts/shake.lua
+++ lua/pac3/core/client/parts/shake.lua
@@ -1,20 +1,28 @@
 local BUILDER, PART = pac.PartTemplate("base_movable")
 
 PART.ClassName = "shake"
-
 PART.Group = 'effects'
 PART.Icon = 'icon16/transmit.png'
 
 BUILDER:StartStorableVars()
-	BUILDER:GetSet("Amplitude", 1)
-	BUILDER:GetSet("Frequency", 1)
-	BUILDER:GetSet("Duration", 0.5)
-	BUILDER:GetSet("Radius", 100)
+	BUILDER:SetPropertyGroup("generic")
+	BUILDER:SetPropertyGroup("shake")
+		BUILDER:GetSet("Amplitude", 1)
+		BUILDER:GetSet("Falloff", false)
+		BUILDER:GetSet("Frequency", 1)
+		BUILDER:GetSet("Duration", 0.5)
+		BUILDER:GetSet("Radius", 100)
 BUILDER:EndStorableVars()
 
 function PART:OnShow(from_rendering)
 	if not from_rendering then
-		util.ScreenShake(self:GetDrawPosition(), self.Amplitude, self.Frequency, math.Clamp(self.Duration, 0.0001, 2), math.Clamp(self.Radius, 0.0001, 500))
+		local position = self:GetDrawPosition()
+		local eyedistance = position:Distance(pac.EyePos)
+		local radius = math.Clamp(self.Radius, 0.0001, 500)
+		
+		local amplitude = (self.Falloff and (1 - (eyedistance / radius)) * self.Amplitude) or (eyedistance < radius and self.Amplitude or 0)
+
+		util.ScreenShake(position, amplitude, self.Frequency, math.Clamp(self.Duration, 0.0001, 2), 0)
 	end
 end
 
