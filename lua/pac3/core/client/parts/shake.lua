3c89e6a10ce7f1895568c1426c3d19eda89e619c
Garrett Brown <grbrown93@sbcglobal.net>
2021-12-27 01:08:49 -0500

    Revert calculation

--- lua/pac3/core/client/parts/shake.lua
+++ lua/pac3/core/client/parts/shake.lua
@@ -20,7 +20,7 @@ function PART:OnShow(from_rendering)
 		local eyedistance = position:Distance(pac.EyePos)
 		local radius = math.Clamp(self.Radius, 0.0001, 500)
 		
-		local amplitude = (self.Falloff and (1 - (eyedistance / radius) ^ 2) * self.Amplitude) or (eyedistance < radius and self.Amplitude or 0)
+		local amplitude = (self.Falloff and (1 - (eyedistance / radius)) * self.Amplitude) or (eyedistance < radius and self.Amplitude or 0)
 
 		util.ScreenShake(position, amplitude, self.Frequency, math.Clamp(self.Duration, 0.0001, 2), 0)
 	end
