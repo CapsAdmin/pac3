64e37d4788982f12dfbd1c6ebb53cb291c13ecf3
Garrett Brown <grbrown93@sbcglobal.net>
2022-02-16 04:02:34 -0500

    Refactor pac3 version. Fixes: #1104 Fixes: #1153

--- lua/autorun/pac_version.lua
+++ lua/autorun/pac_version.lua
@@ -1,92 +1,36 @@
 AddCSLuaFile()
 
-local function dump_version(name, info, verbose)
-	pac.Message(name .. " = " .. info.version_name .. " / " .. info.hash)
-	if verbose then
-		for _, path in ipairs(info.paths) do
-			print("\t" .. path)
-		end
-	end
-end
-
-local function dump(info, verbose)
-	dump_version("Addon", info.addon, verbose)
-	dump_version("Core", info.core, verbose)
-	dump_version("Editor", info.editor, verbose)
-end
-
-if CLIENT then
-	local info
-
-	function _G.PAC_VERSION()
-		return info
-	end
-
-	net.Receive("pac_version", function()
-		info = net.ReadTable()
-	end)
-
-	concommand.Add("pac_version", function(_, _, args)
-		if not info then
-			print("pac version has not been received from server yet")
-			return
-		end
-		dump(info, args[1] == "1")
-	end)
-end
-
 if SERVER then
-	local hash = include("pac3/libraries/hash_version.lua")
+    local function pacVersion()
+        if steamworks.IsSubscribed( "104691717" ) then
+            return "workshop"
+        end
 
-	function _G.PAC_VERSION()
-		return {
-			addon = hash.LuaPaths({
-				"lua/pac3/",
-				"lua/autorun/netstream.lua",
-				"lua/autorun/pac_version.lua",
-				"lua/autorun/pac_core_init.lua",
-				"lua/autorun/pac_editor_init.lua",
-				"lua/autorun/pac_extra_init.lua",
-				"lua/autorun/pac_init.lua",
-				"lua/autorun/pac_init.lua",
-				"lua/entities/gmod_wire_expression2/core/custom/pac.lua",
-			}),
-			editor = hash.LuaPaths({
-				"lua/pac3/editor/",
-			}),
-			core = hash.LuaPaths({
-				"lua/pac3/core/",
-			}),
-		}
-	end
+        for k,v in pairs(select(2, file.Find( "addons/*", "GAME" ))) do
+            if file.Exists("addons/"..v.."/lua/autorun/pac_init.lua", "GAME") then
+                local dir = "addons/"..v.."/.git/"
+                local head = file.Read(dir.."HEAD", "GAME") -- Where head points to
+                if not head then break end
 
-	util.AddNetworkString("pac_version")
+                head = string.match(head, "ref:%s+(%S+)")
+                if not head then break end
 
-	local info = PAC_VERSION()
+                local lastCommit = file.Read( dir..head, "GAME")
+                if not lastCommit then break end
 
-	concommand.Add("pac_calc_version", function(ply, _, args)
-		if not ply:IsAdmin() or not ply:IsValid() then return end
+                return lastCommit
+            end
+        end
 
-		local verbose = args[1] == "1"
-		info = PAC_VERSION()
-
-		net.Start("pac_version")
-			net.WriteTable(info)
-		net.Broadcast()
-
-		dump(info, verbose)
-	end)
-
-	hook.Add("PlayerInitialSpawn", "pac_version", function( ply)
-		local id = "pac_version_" .. pac.Hash(ply)
-		hook.Add("SetupMove", id, function(self, mov, cmd)
-			if self == ply and not cmd:IsForced() then
-				hook.Remove("SetupMove", id)
-
-				net.Start("pac_version")
-					net.WriteTable(info)
-				net.Send(ply)
-			end
-		end)
-	end)
+        return "unknown"
+    end
+    SetGlobalString("pac_version", pacVersion())
 end
+
+function _G.PAC_VERSION()
+    return GetGlobalString("pac_version")
+end
+
+concommand.Add("pac_version", function(_, _, args)
+	print(PAC_VERSION())
+end)
