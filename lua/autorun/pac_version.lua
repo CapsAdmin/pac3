8ab1decf9c6a83cf38c83cc38789d2817da71d91
Garrett Brown <grbrown93@sbcglobal.net>
2022-02-19 12:52:29 -0500

    More info

--- lua/autorun/pac_version.lua
+++ lua/autorun/pac_version.lua
@@ -1,36 +1,46 @@
 AddCSLuaFile()
 
 if SERVER then
-    local function pacVersion()
-        if steamworks.IsSubscribed( "104691717" ) then
-            return "workshop"
-        end
+	local function pacVersion()
+		local addonFound = false
 
-        for k,v in pairs(select(2, file.Find( "addons/*", "GAME" ))) do
-            if file.Exists("addons/"..v.."/lua/autorun/pac_init.lua", "GAME") then
-                local dir = "addons/"..v.."/.git/"
-                local head = file.Read(dir.."HEAD", "GAME") -- Where head points to
-                if not head then break end
+		for k,v in pairs(select(2, file.Find( "addons/*", "GAME" ))) do
+			if file.Exists("addons/"..v.."/lua/autorun/pac_init.lua", "GAME") then
+				addonFound = true
+				local dir = "addons/"..v.."/.git/"
+				local head = file.Read(dir.."HEAD", "GAME") -- Where head points to
+				if not head then break end
 
-                head = string.match(head, "ref:%s+(%S+)")
-                if not head then break end
+				head = string.match(head, "ref:%s+(%S+)")
+				if not head then break end
 
-                local lastCommit = file.Read( dir..head, "GAME")
-                if not lastCommit then break end
+				local lastCommit = string.match(file.Read( dir..head, "GAME") or "", "%S+")
+				if not lastCommit then break end
 
-                return lastCommit
-            end
-        end
+				return "Git: " .. string.GetFileFromFilename(head) .. " (" .. lastCommit .. ")"
+			end
+		end
 
-        return "unknown"
-    end
-    SetGlobalString("pac_version", pacVersion())
+		if addonFound then
+			return "unknown"
+		else
+			return "workshop"
+		end
+	end
+
+	SetGlobalString("pac_version", pacVersion())
 end
 
 function _G.PAC_VERSION()
-    return GetGlobalString("pac_version")
+	return GetGlobalString("pac_version")
 end
 
 concommand.Add("pac_version", function()
 	print(PAC_VERSION())
+	if CLIENT and PAC_VERSION() == "workshop" then
+		print("Fetching workshop info...")
+		steamworks.FileInfo( "104691717", function(result)
+			print("Updated: " .. os.date("%x %X", result.updated))
+		end)
+	end
 end)
