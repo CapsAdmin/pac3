f8728e92b4d662723d81ecd0be301bb648634913
TW1STaL1CKY <tw1stal1cky@gmail.com>
2022-03-18 20:40:41 +0000

    Fixed error caused by entities being deleted within the 3D skybox

--- lua/pac3/core/server/in_skybox.lua
+++ lua/pac3/core/server/in_skybox.lua
@@ -1,19 +1,21 @@
 hook.Add("InitPostEntity","pac_get_sky_camera",function()
 	local sky_camera = ents.FindByClass("sky_camera")[1]
 	if sky_camera then
+		local nwVarName = "pac_in_skybox"
 		local in_skybox = {}
+
 		timer.Create("pac_in_skybox", 0.5, 0, function()
 			local new_in_skybox = {}
 			for _, ent in ipairs(ents.FindInPVS(sky_camera:GetPos())) do
 				if not in_skybox[ent] then
-					ent:SetNW2Bool("pac_in_skybox", true)
+					ent:SetNW2Bool(nwVarName, true)
 				end
 				new_in_skybox[ent] = true
 			end
 
 			for ent in pairs(in_skybox) do
-				if not new_in_skybox[ent] then
-					ent:SetNW2Bool("pac_in_skybox", false)
+				if not new_in_skybox[ent] and ent:IsValid() then
+					ent:SetNW2Bool(nwVarName, false)
 				end
 			end
 
