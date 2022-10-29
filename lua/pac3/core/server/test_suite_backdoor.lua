847c78f3ba336772d1d99f4ff91cb98175e22e27
Redox <mail@redox.to>
2022-10-29 15:05:46 +0200

    Add extra safeguard

--- lua/pac3/core/server/test_suite_backdoor.lua
+++ lua/pac3/core/server/test_suite_backdoor.lua
@@ -1,12 +1,15 @@
 -- this is for the test suite, it is technically a lua run backdoor
--- the test suite is a utility for the pac developers to ensure that pac works and don't break 
+-- the test suite is a utility for the pac developers to ensure that pac works and don't break
 -- after making changes to it
 
 util.AddNetworkString("pac3_test_sutie_backdoor_receive_results")
 util.AddNetworkString("pac3_test_sutie_backdoor")
 
+local enabledConvar = CreateConVar("pac_test_suite_backdoor_enabled", 0, false, false, "Should the pac test suite backdoor be enabled, this allows lua access for superadmins.", 0, 1)
+
 net.Receive("pac3_test_sutie_backdoor", function(len, ply)
 	-- need to be at least super admin
+	if not enabledConvar:GetBool() then return end
 	if not ply:IsSuperAdmin() then return end
 
 	local id = net.ReadString()
