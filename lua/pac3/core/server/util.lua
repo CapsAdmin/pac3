697597f9dcfc6edf29513a21522d280ddec6f0f4
Brandon Sturgeon <brandon@brandonsturgeon.com>
2022-11-21 04:09:22 -0800

    Fix first call of RatelimitAlert (#1264)
    * Fix first call of ratelimit alert

--- lua/pac3/core/server/util.lua
+++ lua/pac3/core/server/util.lua
@@ -51,11 +51,7 @@ function pac.RatelimitAlert( ply, id, message )
 		ply.pac_ratelimit_alerts = {}
 	end
 
-	if not ply.pac_ratelimit_alerts[id] then
-		ply.pac_ratelimit_alerts[id] = CurTime() + 3
-	end
-
-	if CurTime() > ply.pac_ratelimit_alerts[id] then
+	if CurTime() >= ( ply.pac_ratelimit_alerts[id] or 0 ) then
 		ply.pac_ratelimit_alerts[id] = CurTime() + 3
 		if isstring(message) then
 			pac.Message(message)
