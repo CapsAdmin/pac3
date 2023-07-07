c3de119c8a4ca6127f19c6c198161890a6170dc9
Redox <mail@redox.to>
2023-07-07 19:15:57 +0200

    Fix cache.lua style

--- lua/pac3/libraries/urlobj/cache.lua
+++ lua/pac3/libraries/urlobj/cache.lua
@@ -4,7 +4,7 @@ local CACHE = {}
 
 local function CreateCache(cacheId)
 	local cache = {}
-	setmetatable (cache, { __index = CACHE })
+	setmetatable(cache, { __index = CACHE })
 
 	cache:Initialize(cacheId)
 
@@ -14,46 +14,46 @@ end
 function CACHE:Initialize(cacheId)
 	self.Version = 3 -- Update this if the crypto library changes
 
-	self.Path    = "pac3_cache/" .. string.lower (cacheId)
+	self.Path    = "pac3_cache/" .. string.lower(cacheId)
 
-	file.CreateDir (self.Path)
+	file.CreateDir(self.Path)
 end
 
 function CACHE:AddItem(itemId, data)
-	local hash = self:GetItemIdHash (itemId)
+	local hash = self:GetItemIdHash(itemId)
 	local path = self.Path .. "/" .. hash .. ".txt"
-	local key  = self:GetItemIdEncryptionKey (itemId)
+	local key  = self:GetItemIdEncryptionKey(itemId)
 
 	-- Version
-	local f = file.Open (path, "wb", "DATA")
+	local f = file.Open(path, "wb", "DATA")
 	if not f then return end
-	f:WriteLong (self.Version)
+	f:WriteLong(self.Version)
 
 	-- Header
-	local compressedItemId = util.Compress (itemId)
-	local entryItemId = crypto.EncryptString (compressedItemId, key)
-	f:WriteLong (#entryItemId)
-	f:Write (entryItemId, #entryItemId)
+	local compressedItemId = util.Compress(itemId)
+	local entryItemId = crypto.EncryptString(compressedItemId, key)
+	f:WriteLong(#entryItemId)
+	f:Write(entryItemId, #entryItemId)
 
 	-- Data
-	local compressedData = util.Compress (data)
-	data = crypto.EncryptString (compressedData, key)
-	f:WriteLong (#data)
-	f:Write (data, #data)
+	local compressedData = util.Compress(data)
+	data = crypto.EncryptString(compressedData, key)
+	f:WriteLong(#data)
+	f:Write(data, #data)
 
-	f:Close ()
+	f:Close()
 end
 
 function CACHE:Clear()
-	for _, fileName in ipairs(file.Find (self.Path .. "/*", "DATA")) do
-		file.Delete (self.Path .. "/" .. fileName)
+	for _, fileName in ipairs(file.Find(self.Path .. "/*", "DATA")) do
+		file.Delete(self.Path .. "/" .. fileName)
 	end
 end
 
 function CACHE:ClearBefore(time)
 	for _, fileName in ipairs(file.Find(self.Path .. "/*", "DATA")) do
-		if file.Time (self.Path .. "/" .. fileName, "DATA") < time then
-			file.Delete (self.Path .. "/" .. fileName)
+		if file.Time(self.Path .. "/" .. fileName, "DATA") < time then
+			file.Delete(self.Path .. "/" .. fileName)
 		end
 	end
 end
@@ -74,27 +74,27 @@ function CACHE:GetItem(itemId)
 	local key = self:GetItemIdEncryptionKey(itemId)
 
 	-- Version
-	local version = f:ReadLong ()
+	local version = f:ReadLong()
 	if version ~= self.Version then
-		f:Close ()
+		f:Close()
 		return nil
 	end
 
 	-- Header
-	local entryItemIdLength = f:ReadLong ()
-	local entryItemId = crypto.DecryptString (f:Read(entryItemIdLength), key)
+	local entryItemIdLength = f:ReadLong()
+	local entryItemId = crypto.DecryptString(f:Read(entryItemIdLength), key)
 	entryItemId = util.Decompress(entryItemId)
 
 	if itemId ~= entryItemId then
-		f:Close ()
+		f:Close()
 		return nil
 	end
 
 	-- Data
-	local dataLength = f:ReadLong ()
+	local dataLength = f:ReadLong()
 	local data       = f:Read(dataLength, key)
 
-	f:Close ()
+	f:Close()
 
 	data = crypto.DecryptString(data, key)
 	data = util.Decompress(data)
@@ -103,11 +103,11 @@ function CACHE:GetItem(itemId)
 end
 
 function CACHE:GetItemIdEncryptionKey(itemId)
-	return crypto.GenerateKey(string.reverse (itemId))
+	return crypto.GenerateKey(string.reverse(itemId))
 end
 
 function CACHE:GetItemIdHash(itemId)
-	return string.format("%08x", tonumber (util.CRC (itemId)))
+	return string.format("%08x", tonumber(util.CRC(itemId)))
 end
 
 return CreateCache
