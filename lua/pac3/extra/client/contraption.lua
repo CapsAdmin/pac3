e1cbaed3c59eb251faed621faa976c2685fc0f18
Heatwu <87677693+Heatwu@users.noreply.github.com>
2023-08-19 17:28:21 -0700

    Update contraption.lua

--- lua/pac3/extra/client/contraption.lua
+++ lua/pac3/extra/client/contraption.lua
@@ -1,46 +1,107 @@
-local L = pace.LanguageString
+util.AddNetworkString("pac_to_contraption")
 
-pace.AddTool(L"spawn as props", function(part)
-	local data = pacx.PartToContraptionData(part)
-	net.Start("pac_to_contraption")
-		net.WriteTable(data)
-	net.SendToServer()
-end)
+local receieved = {}
 
-function pacx.PartToContraptionData(part, tbl)
-	tbl = tbl or {}
+local function spawn(val,ply)
+	local model = val.mdl
 
-	if part.is_model_part then
-		local data = {}
-
-		local color = part:GetColor()
-		local alpha = part:GetAlpha()
-
-		if part.ProperColorRange then
-			data.clr = Color(color.r * 255, color.g * 255, color.b * 255, alpha * 255)
-		else
-			data.clr = Color(color.r, color.g, color.b, alpha * 255)
-		end
-
-		data.ang = part:GetOwner():GetAngles()
-		data.pos = part:GetOwner():GetPos()
-		data.mat = part:GetMaterial()
-		data.mdl = part:GetModel()
-		data.skn = part:GetSkin()
-
-		local size = part:GetSize()
-		data.scale = part:GetScale()*size
-		
-		data.id = part.UniqueID
-
-		table.insert(tbl, data)
+	if not model or model == "" or model:find("\n") or model:find("..", 1, true) then
+		return
 	end
 
-	for key, part in ipairs(part:GetChildren()) do
-		if part.is_model_part then
-			pacx.PartToContraptionData(part, tbl)
-		end
+	pace.suppress_prop_spawn = true
+	local ok = hook.Run("PlayerSpawnProp", ply, model)
+	pace.suppress_prop_spawn = false
+
+	if not ok then
+		return
 	end
 
-	return tbl
+	local ent = ents.Create("prop_physics")
+
+	SafeRemoveEntity(receieved[val.id])
+	receieved[val.id] = ent
+
+	ent:SetModel(model)-- insecure
+	ent:SetPos(val.pos)-- insecure
+	ent:SetAngles(val.ang)-- insecure
+	ent:SetSkin(val.skn)
+	ent:SetMaterial(val.mat) -- insecure
+	ent:Spawn()
+	ent:SetHealth(9999999)
+	ent:SetColor(val.clr)
+	ent:SetRenderMode( RENDERMODE_TRANSCOLOR )
+
+	hook.Run("PlayerSpawnedProp", ply, model, ent)
+
+	if ent.CPPISetOwner and not (ent:CPPIGetOwner() or NULL):IsValid() then
+		ent:CPPISetOwner(ply)
+	end
+
+	local phys = ent:GetPhysicsObject()
+
+	if phys:IsValid() then
+		phys:EnableMotion(false)
+
+		local maxabs = 150
+
+		val.scale.X = math.Clamp(val.scale.X,-maxabs,maxabs)
+		val.scale.Y = math.Clamp(val.scale.Y,-maxabs,maxabs)
+		val.scale.Z = math.Clamp(val.scale.Z,-maxabs,maxabs)
+
+		for i=0, ent:GetBoneCount()-1 do
+			ent:ManipulateBoneScale( i, val.scale )
+		end
+
+		undo.Create("Prop")
+			undo.SetPlayer(ply)
+			undo.AddEntity(ent)
+		undo.Finish("Prop ("..tostring(model)..")")
+
+		ply:AddCleanup("props", ent)
+
+		gamemode.Call("PlayerSpawnedProp", ply, model, ent)
+	else
+		ent:Remove()
+	end
+
 end
+
+local pac_to_contraption_allow = CreateConVar("pac_to_contraption_allow", "1")
+local max_contraptions = CreateConVar("pac_max_contraption_entities", 60)
+
+pace.PCallNetReceive(net.Receive, "pac_to_contraption", function(len, ply)
+	if not pac_to_contraption_allow:GetBool() then
+		net.Start("pac_submit_acknowledged")
+			net.WriteBool(false)
+			net.WriteString("This server does not allow spawning PAC contraptions.")
+		net.Send(ply)
+
+		return
+	end
+
+	if len < 64 then return end
+
+	local allowed = pac.RatelimitPlayer( ply, "pac_to_contraption", 5, 1, {"Player ", ply, " is spamming pac_to_contraption!"} )
+	if not allowed then return end
+
+	local data = net.ReadTable()
+
+	local max = max_contraptions:GetInt()
+	local count = table.Count(data)
+	if count > max then
+		net.Start("pac_submit_acknowledged")
+			net.WriteBool(false)
+			net.WriteString("You can only spawn " .. max .. " props at a time!")
+		net.Send(ply)
+
+		pac.Message(ply, " might have tried to crash the server by attempting to spawn ", count, " entities with the contraption system!")
+		return
+	end
+
+	pac.Message("Spawning contraption by ", ply, " with ", count, " entities")
+
+	for key, val in pairs(data) do
+		spawn(val,ply)
+	end
+end)
