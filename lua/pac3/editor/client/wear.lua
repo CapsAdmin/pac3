fe187940541f7ba666539743708c01cf51d35d1a
pingu7867 <cedric4321@hotmail.com>
2023-12-20 21:54:31 -0500

    make wear tracker into a reusable function

--- lua/pac3/editor/client/wear.lua
+++ lua/pac3/editor/client/wear.lua
@@ -48,6 +48,18 @@ end
 
 --wearing tracker counter
 pace.still_loading_wearing_count = 0
+--reusable function
+function pace.ExtendWearTracker(duration)
+	if not duration or not isnumber(duration) then duration = 1 end
+	pace.still_loading_wearing = true
+	pace.still_loading_wearing_count = pace.still_loading_wearing_count + 1 --this group is added to the tracked wear count
+	timer.Simple(duration, function()
+		pace.still_loading_wearing_count = pace.still_loading_wearing_count - 1 --assume max 8 seconds to wear
+		if pace.still_loading_wearing_count == 0 then --if this is the last group to wear, we're done
+			pace.still_loading_wearing = false
+		end
+	end)
+end
 
 do -- to server
 	local function net_write_table(tbl)
@@ -96,14 +108,7 @@ do -- to server
 		net.SendToServer()
 		pac.Message(('Transmitting outfit %q to server (%s)'):format(part.Name or part.ClassName or '<unknown>', string.NiceSize(bytes)))
 
-		pace.still_loading_wearing = true
-		pace.still_loading_wearing_count = pace.still_loading_wearing_count + 1 --this group is added to the tracked wear count
-		timer.Simple(8, function()
-			pace.still_loading_wearing_count = pace.still_loading_wearing_count - 1 --assume max 8 seconds to wear
-			if pace.still_loading_wearing_count == 0 then --if this is the last group to wear, we're done
-				pace.still_loading_wearing = false
-			end
-		end)
+		pace.ExtendWearTracker(8)
 
 		return true
 	end
