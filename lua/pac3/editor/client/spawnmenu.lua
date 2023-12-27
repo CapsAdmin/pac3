d76f7c25a6ef02771e70574477a14febf89352e3
pingu7867 <cedric4321@hotmail.com>
2023-12-27 16:41:25 -0500

    Admin utilities in spawnmenu

--- lua/pac3/editor/client/spawnmenu.lua
+++ lua/pac3/editor/client/spawnmenu.lua
@@ -109,6 +109,79 @@ function pace.ClientSettingsMenu(self)
 		self:NumSlider(L"Max render time: ", "pac_max_render_time", 0, 100, 0)
 end
 
+function pace.AdminSettingsMenu(self)
+	if not LocalPlayer():IsAdmin() then return end
+	if not IsValid(self) then return end
+	self:Button("Open PAC3 settings menu (Admin)", "pace_settings")
+
+	self:Help(L"PAC3 outfits: general server policy"):SetFont("DermaDefaultBold")
+		self:NumSlider(L"Server Draw distance:", "pac_sv_draw_distance", 0, 20000, 0)
+		self:CheckBox(L"Prevent spam with pac_submit", "pac_submit_spam")
+		self:CheckBox(L"Players need to +USE on others to reveal outfits", "pac_onuse_only_force")
+		self:CheckBox(L"Restrict editor camera", "pac_restrictions")
+		self:CheckBox(L"Allow MDL zips", "pac_allow_mdl")
+		self:CheckBox(L"Allow MDL zips for entity", "pac_allow_mdl_entity")
+		self:CheckBox(L"Allow entity model modifier", "pac_modifier_model")
+		self:CheckBox(L"Allow entity size modifier", "pac_modifier_size")
+		self:CheckBox(L"Allow blood color modifier", "pac_allow_blood_color")
+	self:Help(""):SetFont("DermaDefaultBold")--spacers
+	self:Help(""):SetFont("DermaDefaultBold")
+
+	self:Help(L"PAC3 combat: general server policy"):SetFont("DermaDefaultBold")
+		self:NumSlider(L"Rate limiter", "pac_sv_combat_enforce_netrate", 0, 1000, 0)
+		self:NumSlider(L"Distance limiter", "pac_sv_combat_distance_enforced", 0, 64000, 0)
+		self:NumSlider(L"Allowance, in number of messages", "pac_sv_combat_enforce_netrate_buffersize", 0, 400, 0)
+		self:CheckBox(L"Use general prop protection based on player consents", "pac_sv_prop_protection")
+		self:NumSlider(L"Entity limit per combat action", "pac_sv_entity_limit_per_combat_operation", 0, 1000, 0)
+		self:NumSlider(L"Entity limit per player", "pac_sv_entity_limit_per_player_per_combat_operation", 0, 500, 0)
+		self:CheckBox(L"Only specifically allowed users can do pac3 combat actions", "pac_sv_combat_whitelisting")
+	self:Help(""):SetFont("DermaDefaultBold")--spacers
+	self:Help(""):SetFont("DermaDefaultBold")
+		
+	self:Help(L"Combat parts (more detailed settings in the full editor settings menu)"):SetFont("DermaDefaultBold")
+		self:Help(L"Damage Zones"):SetFont("DermaDefaultBold")
+		self:CheckBox(L"Enable damage zones", "pac_sv_damage_zone")
+		self:NumSlider(L"Max damage", "pac_sv_damage_zone_max_damage", 0, 268435455, 0)
+		self:NumSlider(L"Max radius", "pac_sv_damage_zone_max_radius", 0, 32767, 0)
+		self:NumSlider(L"Max length", "pac_sv_damage_zone_max_length", 0, 32767, 0)
+		self:CheckBox(L"Enable damage zone dissolve", "pac_sv_damage_zone_allow_dissolve")
+		
+		self:Help(L"Hitscan"):SetFont("DermaDefaultBold")
+		self:CheckBox(L"Enable hitscan part", "pac_sv_hitscan")
+		self:NumSlider(L"Max damage", "pac_sv_hitscan_max_damage", 0, 268435455, 0)
+		self:CheckBox(L"Force damage division among multi-shot bullets", "pac_sv_hitscan_divide_max_damage_by_max_bullets")
+
+		self:Help(L"Lock part"):SetFont("DermaDefaultBold")
+		self:CheckBox(L"Enable lock part", "pac_sv_lock")
+		self:CheckBox(L"Allow grab", "pac_sv_lock_grab")
+		self:CheckBox(L"Allow teleport", "pac_sv_lock_teleport")
+		
+		self:Help(L"Force part"):SetFont("DermaDefaultBold")
+		self:CheckBox(L"Enable force part", "pac_sv_force")
+		self:NumSlider(L"Max amount", "pac_sv_force_max_amount", 0, 10000000, 0)
+		self:NumSlider(L"Max radius", "pac_sv_force_max_radius", 0, 32767, 0)
+		self:NumSlider(L"Max length", "pac_sv_force_max_length", 0, 32767, 0)
+		
+		self:Help(L"Force part"):SetFont("DermaDefaultBold")
+		self:CheckBox(L"Enable health modifier", "pac_sv_health_modifier")
+		self:CheckBox(L"Allow changing max health or armor", "pac_sv_health_modifier_allow_maxhp")
+		self:NumSlider(L"Minimum combined damage scaling", "pac_sv_health_modifier_min_damagescaling", -10, 1, 2)
+		self:CheckBox(L"Allow extra health bars", "pac_sv_health_modifier_extra_bars")
+		
+		self:Help(L"Projectile part"):SetFont("DermaDefaultBold")
+		self:CheckBox(L"Enable physical projectiles", "pac_sv_projectiles")
+		self:CheckBox(L"Enable custom collide meshes for physical projectiles", "pac_sv_projectile_allow_custom_collision_mesh")
+		self:NumSlider(L"Max speed", "pac_sv_force_max_amount", 0, 5000, 0)
+		self:NumSlider(L"Max physical radius", "pac_sv_projectile_max_phys_radius", 0, 4095, 0)
+		self:NumSlider(L"Max damage radius", "pac_sv_projectile_max_damage_radius", 0, 4095, 0)
+
+		self:Help(L"Player movement part"):SetFont("DermaDefaultBold")
+		self:CheckBox(L"Allow playermovement", "pac_free_movement")
+		self:CheckBox(L"Allow playermovement mass", "pac_player_movement_allow_mass")
+		self:CheckBox(L"Allow physics damage scaling by mass", "pac_player_movement_physics_damage_scaling")
+		
+end
+
 
 
 local icon_cvar = CreateConVar("pac_icon", "0", {FCVAR_ARCHIVE}, "Use the new PAC4.5 icon or the old PAC icon.\n0 = use the old one\n1 = use the new one")
@@ -178,6 +251,17 @@ hook.Add("PopulateToolMenu", "pac_spawnmenu", function()
 		"",
 		"",
 		pace.ClientSettingsMenu,
+		{
+		}
+	)
+	spawnmenu.AddToolMenuOption(
+		"Utilities",
+		"PAC",
+		"PAC3Admin",
+		L"Admin",
+		"",
+		"",
+		pace.AdminSettingsMenu,
 		{
 		}
 	)
