03603bf9191077ecfd821423c800b22b9708730c
Ōkami Adam Sila Kann <17802255+WolfKann@users.noreply.github.com>
2024-04-02 21:07:12 -0400

    Add functions into event.lua
    This adds "Is_No_Targeted", "Is_No_Draw" and get network int/bool/string/float variables into event.lua

--- lua/pac3/core/client/parts/event.lua
+++ lua/pac3/core/client/parts/event.lua
@@ -440,6 +440,52 @@ PART.OldEvents = {
 		end,
 	},
 
+	is_no_draw = {
+		callback = function(self, ent)
+			ent = try_viewmodel(ent)
+			return ent:GetNoDraw()
+		end,
+	},
+	
+	is_no_target = {
+		callback = function(self, ent)
+			ent = try_viewmodel(ent)
+			return ent:IsFlagSet( FL_NOTARGET )
+		end,
+	},
+
+	get_networked_string = {
+		arguments = {{name = "string"}, {result = "string"}},
+		callback = function(self, ent, name, result)
+				ent = try_viewmodel(ent)
+				return ent:GetNWString(tostring(name)) == tostring(result)
+		end,
+	},
+	
+	get_networked_int = {
+		arguments = {{name = "string"}, {num = "number"}},
+		callback = function(self, ent, name, num)
+				ent = try_viewmodel(ent)
+				return self:NumberOperator(ent:GetNWInt(tostring(name), num))
+		end,
+	},
+
+	get_networked_bool = {
+		arguments = {{name = "string"}, {bool = "string"}},
+		callback = function(self, ent, name, bool)
+				ent = try_viewmodel(ent)
+				return ent:GetNWBool(tostring(name)) == tobool(bool)
+		end,
+	},
+	
+	get_networked_float = {
+		arguments = {{name = "string"}, {float = "number"}},
+		callback = function(self, ent, name, float)
+				ent = try_viewmodel(ent)
+				return self:NumberOperator(ent:GetNWFloat(tostring(name), float))
+		end,
+	},
+	
 	is_on_fire = {
 		callback = function(self, ent)
 			ent = try_viewmodel(ent)
