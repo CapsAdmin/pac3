7a62471c7e488ec9d4819179fe64af3d5ed0edd0
Denneisk <20892685+Denneisk@users.noreply.github.com>
2024-04-12 20:20:02 -0400

    Combine to single networkstring Decrease limit to 2048

--- lua/entities/gmod_wire_expression2/core/custom/pac.lua
+++ lua/entities/gmod_wire_expression2/core/custom/pac.lua
@@ -1,14 +1,11 @@
 E2Lib.RegisterExtension("pac", true)
 
-util.AddNetworkString("pac_e2_setkeyvalue_str")
-util.AddNetworkString("pac_e2_setkeyvalue_num")
-util.AddNetworkString("pac_e2_setkeyvalue_vec")
-util.AddNetworkString("pac_e2_setkeyvalue_ang")
+util.AddNetworkString("pac_e2_setkeyvalue")
 
 local enabledConvar = CreateConVar("pac_e2_ratelimit_enable", "1", {FCVAR_ARCHIVE}, "If the e2 ratelimit should be enabled.", 0, 1)
 local rate = CreateConVar("pac_e2_ratelimit_refill", "0.025", {FCVAR_ARCHIVE}, "The speed at which the ratelimit buffer refills.", 0, 1000)
 local buffer = CreateConVar("pac_e2_ratelimit_buffer", "300", {FCVAR_ARCHIVE}, "How large the ratelimit buffer should be.", 0, 1000)
-local bytes = CreateConVar("pac_e2_bytelimit", "8192", FCVAR_ARCHIVE, "Limit number of bytes sent in a single tick.", 0, 65532)
+local bytes = CreateConVar("pac_e2_bytelimit", "2048", FCVAR_ARCHIVE, "Limit number of bytes sent in a single tick.", 0, 65532)
 
 local byteLimits = WireLib.RegisterPlayerTable()
 local function canRunFunction(self, g, k, v)
@@ -35,50 +32,57 @@ local function canRunFunction(self, g, k, v)
 	return true
 end
 
+--- Domain-specific type IDs for networking E2 keyvalues
+---@alias pac.E2.NetID
+---| 0 # String
+---| 1 # Number
+---| 2 # Vector
+---| 3 # Angle
+
 e2function void pacSetKeyValue(entity owner, string global_id, string key, string value)
 	if not canRunFunction(self, global_id, key, value) then return end
-	net.Start("pac_e2_setkeyvalue_str")
+	net.Start("pac_e2_setkeyvalue")
 		net.WriteEntity(self.player)
 		net.WriteEntity(owner)
 		net.WriteString(global_id)
 		net.WriteString(key)
-
+		net.WriteUInt(0, 2)
 		net.WriteString(value)
 	net.Broadcast()
 end
 
 e2function void pacSetKeyValue(entity owner, string global_id, string key, number value)
 	if not canRunFunction(self, global_id, key, "nmbr") then return end -- Workaround because I don't want to add cases for each type, 4 bytes
-	net.Start("pac_e2_setkeyvalue_num")
+	net.Start("pac_e2_setkeyvalue")
 		net.WriteEntity(self.player)
 		net.WriteEntity(owner)
 		net.WriteString(global_id)
 		net.WriteString(key)
-
+		net.WriteUInt(1, 2)
 		net.WriteFloat(value)
 	net.Broadcast()
 end
 
 e2function void pacSetKeyValue(entity owner, string global_id, string key, vector value)
 	if not canRunFunction(self, global_id, key, "vctrvctrvctr") then return end -- 4 bytes, 3 times
-	net.Start("pac_e2_setkeyvalue_vec")
+	net.Start("pac_e2_setkeyvalue")
 		net.WriteEntity(self.player)
 		net.WriteEntity(owner)
 		net.WriteString(global_id)
 		net.WriteString(key)
-
+		net.WriteUInt(2, 2)
 		net.WriteVector(value)
 	net.Broadcast()
 end
 
 e2function void pacSetKeyValue(entity owner, string global_id, string key, angle value)
 	if not canRunFunction(self, global_id, key, "vctrvctrvctr") then return end
-	net.Start("pac_e2_setkeyvalue_ang")
+	net.Start("pac_e2_setkeyvalue")
 		net.WriteEntity(self.player)
 		net.WriteEntity(owner)
 		net.WriteString(global_id)
 		net.WriteString(key)
-
+		net.WriteUInt(3, 2)
 		net.WriteAngle(value)
 	net.Broadcast()
 end
