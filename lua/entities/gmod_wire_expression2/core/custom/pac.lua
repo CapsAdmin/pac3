c8923feea542f202ee4ea98e747ba00505861d8d
Denneisk <20892685+Denneisk@users.noreply.github.com>
2024-04-14 12:21:38 -0400

    Change limit to be per second Make messages unreliable

--- lua/entities/gmod_wire_expression2/core/custom/pac.lua
+++ lua/entities/gmod_wire_expression2/core/custom/pac.lua
@@ -2,22 +2,25 @@ E2Lib.RegisterExtension("pac", true)
 
 util.AddNetworkString("pac_e2_setkeyvalue")
 
-local enabledConvar = CreateConVar("pac_e2_ratelimit_enable", "1", {FCVAR_ARCHIVE}, "If the e2 ratelimit should be enabled.", 0, 1)
-local rate = CreateConVar("pac_e2_ratelimit_refill", "0.025", {FCVAR_ARCHIVE}, "The speed at which the ratelimit buffer refills.", 0, 1000)
-local buffer = CreateConVar("pac_e2_ratelimit_buffer", "300", {FCVAR_ARCHIVE}, "How large the ratelimit buffer should be.", 0, 1000)
-local bytes = CreateConVar("pac_e2_bytelimit", "2048", FCVAR_ARCHIVE, "Limit number of bytes sent in a single tick.", 0, 65532)
+local enabledConvar = CreateConVar("pac_e2_ratelimit_enable", "1", FCVAR_ARCHIVE, "If the e2 ratelimit should be enabled.", 0, 1)
+local rate = CreateConVar("pac_e2_ratelimit_refill", "0.025", FCVAR_ARCHIVE, "The speed at which the ratelimit buffer refills.", 0, 1000)
+local buffer = CreateConVar("pac_e2_ratelimit_buffer", "300", FCVAR_ARCHIVE, "How large the ratelimit buffer should be.", 0, 1000)
+local bytes = CreateConVar("pac_e2_bytelimit", "2048", FCVAR_ARCHIVE, "Limit number of bytes sent per second for PAC E2 messages.", 0, 65532)
 
 local byteLimits = WireLib.RegisterPlayerTable()
 local function canRunFunction(self, g, k, v)
 	local byteLimit = byteLimits[self.player]
+	local ct = CurTime()
 	if not byteLimit then
-		byteLimit = { CurTime(), 0 }
+		byteLimit = { ct + 1, 0 }
 		byteLimits[self.player] = byteLimit
 	end
 
 	local lim = #g + #k + #v
-	if byteLimit[1] == CurTime() then
+	if ct < byteLimit[1] then
 		lim = lim + byteLimit[2]
+	else
+		byteLimit[1] = ct + 1
 	end
 	byteLimit[2] = lim
 
@@ -41,7 +44,7 @@ end
 
 e2function void pacSetKeyValue(entity owner, string global_id, string key, string value)
 	if not canRunFunction(self, global_id, key, value) then return end
-	net.Start("pac_e2_setkeyvalue")
+	net.Start("pac_e2_setkeyvalue", true)
 		net.WriteEntity(self.player)
 		net.WriteEntity(owner)
 		net.WriteString(global_id)
@@ -53,7 +56,7 @@ end
 
 e2function void pacSetKeyValue(entity owner, string global_id, string key, number value)
 	if not canRunFunction(self, global_id, key, "nmbr") then return end -- Workaround because I don't want to add cases for each type, 4 bytes
-	net.Start("pac_e2_setkeyvalue")
+	net.Start("pac_e2_setkeyvalue", true)
 		net.WriteEntity(self.player)
 		net.WriteEntity(owner)
 		net.WriteString(global_id)
@@ -65,7 +68,7 @@ end
 
 e2function void pacSetKeyValue(entity owner, string global_id, string key, vector value)
 	if not canRunFunction(self, global_id, key, "vctrvctrvctr") then return end -- 4 bytes, 3 times
-	net.Start("pac_e2_setkeyvalue")
+	net.Start("pac_e2_setkeyvalue", true)
 		net.WriteEntity(self.player)
 		net.WriteEntity(owner)
 		net.WriteString(global_id)
@@ -77,7 +80,7 @@ end
 
 e2function void pacSetKeyValue(entity owner, string global_id, string key, angle value)
 	if not canRunFunction(self, global_id, key, "vctrvctrvctr") then return end
-	net.Start("pac_e2_setkeyvalue")
+	net.Start("pac_e2_setkeyvalue", true)
 		net.WriteEntity(self.player)
 		net.WriteEntity(owner)
 		net.WriteString(global_id)
