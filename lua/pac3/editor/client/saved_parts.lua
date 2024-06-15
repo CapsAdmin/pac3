11acae243e49f7cadeb29a1055b5db0dc4fdf26f
techbot <techbot121@gmail.com>
2024-06-15 12:22:02 +0200

    make it more clear that you can load only outfits via "load from url"

--- lua/pac3/editor/client/saved_parts.lua
+++ lua/pac3/editor/client/saved_parts.lua
@@ -97,7 +97,7 @@ end
 local last_backup
 local maxBackups = CreateConVar("pac_backup_limit", "100", {FCVAR_ARCHIVE}, "Maximal amount of backups")
 local autoload_prompt = CreateConVar("pac_prompt_for_autoload", "1", {FCVAR_ARCHIVE}, "Whether to ask before loading autoload. The prompt can let you choose to not load, pick autoload or the newest backup")
-local auto_spawn_prop = CreateConVar("pac_autoload_preferred_prop", "2", {FCVAR_ARCHIVE}, "When loading a pac with an owner name suggesting a prop, notify you and then wait before auto-applying the outfit next time you spawn a prop.\n"..
+local auto_spawn_prop = CreateConVar("pac_autoload_preferred_prop", "2", {FCVAR_ARCHIVE}, "When loading a pac with an owner name suggesting a prop, notify you and then wait before auto-applying the outfit next time you spawn a prop.\n" ..
 																								"0 : do not check\n1 : check if only 1 such group is present\n2 : check if multiple such groups are present and queue one group at a time")
 
 
@@ -137,7 +137,7 @@ function pace.Backup(data, name)
 		local str = pace.luadata.Encode(data)
 
 		if str ~= last_backup then
-			file.Write("pac3/__backup/" .. (name=="" and name or (name..'_')) .. date .. ".txt", str)
+			file.Write("pac3/__backup/" .. (name == "" and name or (name .. "_")) .. date .. ".txt", str)
 			last_backup = str
 		end
 	end
@@ -202,7 +202,7 @@ function pace.LoadParts(name, clear, override_part)
 
 	else
 		if name ~= "autoload.txt" and not string.find(name, "pac3/__backup") then
-			if file.Exists("pac3/" .. name..".txt", "DATA") then
+			if file.Exists("pac3/" .. name .. ".txt", "DATA") then
 				cookie.Set( "pac_last_loaded_outfit", name .. ".txt" )
 			end
 		end
@@ -213,36 +213,43 @@ function pace.LoadParts(name, clear, override_part)
 		pac.dprint("loading Parts %s", name)
 
 		if name:find("https?://") then
-			local function callback(str)
-				if string.find( str, "<!DOCTYPE html>" ) then
-					pace.MessagePrompt("Invalid URL, the website returned a HTML file. If you're using Github then use the RAW option.", "URL Failed", "OK")
-					return
+
+			local ext = name:match("/.+%.(%a+)[%?&]?.-")
+			if ext == "txt" then
+				local function callback(str)
+					if string.find( str, "<!DOCTYPE html>" ) then
+						pace.MessagePrompt("Invalid URL, the website returned a HTML file. If you're using Github then use the RAW option.", "URL Failed", "OK")
+						return
+					end
+
+					local data, err = pace.luadata.Decode(str)
+					if not data then
+						ErrorNoHalt(("URL fail: %s : %s\n"):format(name, err))
+						local message = string.format("URL fail: %s : %s\n", name, err)
+						pace.MessagePrompt(message, "URL Failed", "OK")
+						return
+					end
+
+					pace.LoadPartsFromTable(data, clear, override_part)
 				end
 
-				local data, err = pace.luadata.Decode(str)
-				if not data then
-					ErrorNoHalt(("URL fail: %s : %s\n"):format(name,err))
-					local message = string.format("URL fail: %s : %s\n", name, err)
-					pace.MessagePrompt(message, "URL Failed", "OK")
-					return
-				end
-
-				pace.LoadPartsFromTable(data, clear, override_part)
+				pac.HTTPGet(name, callback, function(err)
+					pace.MessagePrompt(err, "HTTP Request Failed for " .. name, "OK")
+				end)
+			else
+				pace.MessagePrompt("Invalid file", ".txt file expected, got" .. ext, "OK")
+				return
 			end
-
-			pac.HTTPGet(name, callback, function(err)
-				pace.MessagePrompt(err, "HTTP Request Failed for " .. name, "OK")
-			end)
 		else
 			name = name:gsub("%.txt", "")
 
-			local data,err = pace.luadata.ReadFile("pac3/" .. name .. ".txt")
+			local data, err = pace.luadata.ReadFile("pac3/" .. name .. ".txt")
 			local has_possible_prop_pacs = false
 
 			if data and istable(data) then
-				for i,part in pairs(data) do
-					if part.self then
-						if isnumber(tonumber(part.self.OwnerName)) then has_possible_prop_pacs = true end
+				for i, part in pairs(data) do
+					if part.self and isnumber(tonumber(part.self.OwnerName)) then
+						has_possible_prop_pacs = true
 					end
 				end
 			end
@@ -253,7 +260,7 @@ function pace.LoadParts(name, clear, override_part)
 				LocalPlayer().pac_propload_queuedparts = LocalPlayer().pac_propload_queuedparts or {}
 
 				--check all root parts from data. format: each data member is a {self, children} table of the part and the list of children
-				for i,part in pairs(data) do
+				for i, part in pairs(data) do
 					local possible_prop_pac = isnumber(tonumber(part.self.OwnerName))
 					if part.self.ClassName == "group" and possible_prop_pac then
 
@@ -278,7 +285,7 @@ function pace.LoadParts(name, clear, override_part)
 
 				if name == "autoload" and (not data or not next(data)) then
 					local err
-					data,err = pace.luadata.ReadFile("pac3/sessions/" .. name .. ".txt",nil,true)
+					data, err = pace.luadata.ReadFile("pac3/sessions/" .. name .. ".txt", nil, true)
 					if not data then
 						if err then
 							ErrorNoHalt(("Autoload failed: %s\n"):format(err))
@@ -286,7 +293,7 @@ function pace.LoadParts(name, clear, override_part)
 						return
 					end
 				elseif not data then
-					ErrorNoHalt(("Decoding %s failed: %s\n"):format(name,err))
+					ErrorNoHalt(("Decoding %s failed: %s\n"):format(name, err))
 					return
 				end
 
@@ -299,11 +306,11 @@ function pace.LoadParts(name, clear, override_part)
 	end
 end
 
-concommand.Add('pac_load_url', function(ply, cmd, args)
-	if not args[1] then return print('[PAC3] No URL specified') end
+concommand.Add("pac_load_url", function(ply, cmd, args)
+	if not args[1] then return print("[PAC3] No URL specified") end
 	local url = args[1]:Trim()
-	if not url:find("https?://") then return print('[PAC3] Invalid URL specified') end
-	pac.Message('Loading specified URL')
+	if not url:find("https?://") then return print("[PAC3] Invalid URL specified") end
+	pac.Message("Loading specified URL")
 	if args[2] == nil then args[2] = '1' end
 	pace.LoadParts(url, tobool(args[2]))
 end)
@@ -348,8 +355,8 @@ function pace.LoadPartsFromTable(data, clear, override_part)
 	pace.RefreshTree(true)
 
 	for i, part in ipairs(partsLoaded) do
-		part:CallRecursive('OnOutfitLoaded')
-		part:CallRecursive('PostApplyFixes')
+		part:CallRecursive("OnOutfitLoaded")
+		part:CallRecursive("PostApplyFixes")
 	end
 
 	pac.LocalPlayer.pac_fix_show_from_render = SysTime() + 1
@@ -362,10 +369,9 @@ local function add_files(tbl, dir)
 
 	if folders then
 		for key, folder in pairs(folders) do
-			if folder == "__backup" or folder == "objcache" or folder == "__animations" or folder == "__backup_save" then goto CONTINUE end
+			if folder == "__backup" or folder == "objcache" or folder == "__animations" or folder == "__backup_save" then continue end
 			tbl[folder] = {}
 			add_files(tbl[folder], dir .. "/" .. folder)
-			::CONTINUE::
 		end
 	end
 
@@ -385,13 +391,13 @@ local function add_files(tbl, dir)
 						data.Path = path
 						data.RelativePath = (dir .. "/" .. data.Name):sub(2)
 
-					local dat,err=pace.luadata.ReadFile(path)
+					local dat, err = pace.luadata.ReadFile(path)
 						data.Content = dat
 
 					if dat then
 						table.insert(tbl, data)
 					else
-						pac.dprint(("Decoding %s failed: %s\n"):format(path,err))
+						pac.dprint(("Decoding %s failed: %s\n"):format(path, err))
 						chat.AddText(("Could not load: %s\n"):format(path))
 					end
 
@@ -400,7 +406,7 @@ local function add_files(tbl, dir)
 		end
 	end
 
-	table.sort(tbl, function(a,b)
+	table.sort(tbl, function(a, b)
 		if a.Time and b.Time then
 			return a.Name < b.Name
 		end
@@ -457,7 +463,7 @@ end
 local function populate_parts(menu, tbl, override_part, clear)
 	for key, data in pairs(tbl) do
 		if not data.Path then
-			local menu, pnl = menu:AddSubMenu(key, function()end, data)
+			local menu, pnl = menu:AddSubMenu(key, function() end, data)
 			pnl:SetImage(pace.MiscIcons.load)
 			menu.GetDeleteSelf = function() return false end
 			local old = menu.Open
@@ -503,14 +509,14 @@ function pace.AddOneDirectorySavedPartsToMenu(menu, subdir, nicename)
 	if not subdir then return end
 	local subdir_head = subdir .. "/"
 
-	local exp_submenu, pnl = menu:AddSubMenu(L""..subdir)
+	local exp_submenu, pnl = menu:AddSubMenu(L"" .. subdir)
 	pnl:SetImage(pace.MiscIcons.load)
 	exp_submenu.GetDeleteSelf = function() return false end
 	subdir = "pac3/" .. subdir
 	if nicename then exp_submenu:SetText(nicename) end
 
 	add_expensive_submenu_load(pnl, function(subdir)
-		local files = file.Find(subdir.."/*", "DATA")
+		local files = file.Find(subdir .. "/*", "DATA")
 		local files2 = {}
 		--PrintTable(files)
 		for i, filename in ipairs(files) do
@@ -554,7 +560,7 @@ function pace.AddSavedPartsToMenu(menu, clear, override_part)
 			"",
 
 			function(name)
-				local data,err = pace.luadata.Decode(name)
+				local data, _ = pace.luadata.Decode(name)
 				if data then
 					pace.LoadPartsFromTable(data, clear, override_part)
 				end
@@ -568,7 +574,7 @@ function pace.AddSavedPartsToMenu(menu, clear, override_part)
 		examples.GetDeleteSelf = function() return false end
 
 		local sorted = {}
-		for k,v in pairs(pace.example_outfits) do sorted[#sorted + 1] = {k = k, v = v} end
+		for k, v in pairs(pace.example_outfits) do sorted[#sorted + 1] = {k = k, v = v} end
 		table.sort(sorted, function(a, b) return a.k < b.k end)
 
 		for _, data in pairs(sorted) do
@@ -681,7 +687,7 @@ local function populate_parts(menu, tbl, dir, override_part)
 	menu:AddSpacer()
 	for key, data in pairs(tbl) do
 		if not data.Path then
-			local menu, pnl = menu:AddSubMenu(key, function()end, data)
+			local menu, pnl = menu:AddSubMenu(key, function() end, data)
 			pnl:SetImage(pace.MiscIcons.load)
 			menu.GetDeleteSelf = function() return false end
 			populate_parts(menu, data, dir .. "/" .. key, override_part)
@@ -718,11 +724,11 @@ local function populate_parts(menu, tbl, dir, override_part)
 					local function delete_directory(dir)
 						local files, folders = file.Find(dir .. "*", "DATA")
 
-						for k,v in ipairs(files) do
+						for k, v in ipairs(files) do
 							file.Delete(dir .. v)
 						end
 
-						for k,v in ipairs(folders) do
+						for k, v in ipairs(folders) do
 							delete_directory(dir .. v .. "/")
 						end
 
@@ -818,7 +824,7 @@ function pace.FixBadGrouping(data)
 			},
 		}
 
-		for k,v in pairs(other) do
+		for k, v in pairs(other) do
 			table.insert(out, v)
 		end
 
